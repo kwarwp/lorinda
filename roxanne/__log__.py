73d79e4f11401ac510ded5814354ab3111461ea4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-04-14 22:02:48 -0300

    Created roxanne/__log__.py @2020-04-15 01:02

---
+++ roxanne/__log__.py
@@ -0,0 +1,19 @@
+
+{'date': 'Tue Apr 14 2020 22:02:43.621 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 10
+    class jujuba():
+  module <module> line 22
+    jujuba()
+NameError: name 'jujuba' is not defined
+'''},
