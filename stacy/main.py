301385874a3a49756d205b3ba7fafd9dcd024f86
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-04-17 18:46:53 -0300

    Saved stacy/main.py @2020-04-17 21:46

--- stacy/main.py
+++ stacy/main.py
@@ -3,4 +3,4 @@ from _spy.vitollino.main import Cena,Elemento,Texto
 from _spy.vitollino.main import Inventario as inv
 
 RUA = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimage.freepik.com%2Fvetores-gratis%2Filustracao-de-rua-da-cidade_1284-10867.jpg&imgrefurl=https%3A%2F%2Fbr.freepik.com%2Fvetores-gratis%2Filustracao-de-rua-da-cidade_3817795.htm&tbnid=CQcjp8wYdbeSDM&vet=12ahUKEwjShJ_0s_DoAhWXCrkGHaZyBtEQMygDegUIARDnAQ..i&docid=ch1VsUym1oK8iM&w=626&h=626&hl=pt-BR&safe=images&ved=2ahUKEwjShJ_0s_DoAhWXCrkGHaZyBtEQMygDegUIARDnAQ"
-
+PLAYER = 
