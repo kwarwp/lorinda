baa1d2bec07a5035149eb7d5db0926c451458520
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-04-30 20:34:41 -0300

    Saved parisa/__log__.py @2020-04-30 23:34

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Thu Apr 30 2020 20:34:41.396 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  class oi():
+  ^
+IndentationError: unexpected indent
+'''},
