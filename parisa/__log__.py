8c54addd335c000d702383339a04af7b5640a204
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-04-30 20:34:34 -0300

    Created parisa/__log__.py @2020-04-30 23:34

---
+++ parisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Apr 30 2020 20:34:28.430 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  class oi()
+  ^
+IndentationError: unexpected indent
+'''},
