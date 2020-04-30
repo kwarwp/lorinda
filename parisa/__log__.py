835dbae13f82ab984cfba91abca2fff86d003751
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-04-30 20:51:13 -0300

    Saved parisa/__log__.py @2020-04-30 23:51

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -105,3 +105,23 @@ AttributeError: 'oi' object has no attribute 'saladeaula'
     self.quadro.entra(sala1)
 NameError: name 'sala1' is not defined
 '''},
+{'date': 'Thu Apr 30 2020 20:51:11.727 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 20
+    oi()
+  module <module> line 15
+    self.quadro.entra(SALA)
+  module _spy.vitollino.main line 472
+    self.scorer.update(valor=cena.nome, move=self.xy,
+AttributeError: 'str' object has no attribute 'nome'
+'''},
