e2362076393b9e5194d25722ca7ac0af65074e75
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-01 18:40:28 -0300

    Created kathryn/main.py @2020-05-01 21:40

---
+++ kathryn/main.py
@@ -0,0 +1,13 @@
+# lorinda.kathryn.main.py
+from_supy.vitolino.main.import Cena,Elemento,Texto, STYLE
+form_supy.vitolino.main.import Inventario as inv
+
+STYLE["Width"] = 800
+STYLE["height"] = "600px"
+
+SALA FIOCRUZ = "http://supygirls.pythonanywhere.com"
+NPC = " https://i.imgur.com/NEG2o8W.jpg"
+PERGAMINHO = "https://i.imgur.com/Xe3zdhP.jpg"
+PESSOA BRANCA CABELO LISO OLHOS VERDES = "https://i.imgur.com/6vgubFm.jpg"
+PESSOA NEGRA CABELO BLACK OLHOS CASTANHOS = "https://i.imgur.com/wtpBiVS.jpg"
+PESSOA RUIVA PELE BRANCA CABELO VERMELHO = "https://i.imgur.com/ZcfLShe.jpg"
