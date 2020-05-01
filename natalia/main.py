866254d705767c9efc1d1b248fa4637c04212be9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-01 01:03:03 -0300

    Saved natalia/main.py @2020-05-01 04:03

--- natalia/main.py
+++ natalia/main.py
@@ -8,6 +8,7 @@ STYLE["height"] = "600px"
 ZEZINHO = "https://i.imgur.com/94lhgKo.png"
 ROSALINDA = "https://i.imgur.com/qvuwHvs.png"
 CASA = "https://www.tudoconstrucao.com/wp-content/uploads/2014/12/casa-de-praia-colorida-simples.jpg"
+QUADROS = "https://staticcdns3.bidu.com.br/jamal/uploads/2016/11/06164925/3b1.jpg"
 
 class gameg():
 
@@ -18,6 +19,14 @@ class gameg():
         style=dict (left=400, top=350, width=300, height="200px",))
         zezinho.entra(casa)
         rosalinda.entra(casa)
+        
+        quadros = Cena( img = QUADROS)
+        casa.direita=quadros
+        zezinho.entra(quadros)#colocar mesnagem confusa de zezinho em relação aos quadros
+        rosalinda.entra(quadros)
+        
+        
+        
         
         casa.vai()
 gameg()        
