502b2429332acbfc3535609173e79f1705809040
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-04-30 23:42:23 -0300

    Created natalia/main.py @2020-05-01 02:42

---
+++ natalia/main.py
@@ -0,0 +1,6 @@
+# lorinda.natalia.main.py Leniaaaaah(não mexer)
+from _spy.vitollino.main import Cena,Elemento,Texto,STYLE
+from _spy.vitollino.main import Inventario as inv
+
+STYLE["width"] = 800
+STYLE["height"] = "600px"
