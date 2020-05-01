937bd951936802e846435b400fd58ec51c1f4183
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-04-30 21:18:03 -0300

    Saved parisa/__log__.py @2020-05-01 00:18

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -143,3 +143,21 @@ AttributeError: 'str' object has no attribute 'nome'
     self.textotalita3.foi = self.habilita
 AttributeError: 'oi' object has no attribute 'habilita'
 '''},
+{'date': 'Thu Apr 30 2020 21:18:02.214 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 22
+    oi()
+  module <module> line 16
+    self.quadro.entra(sala1)
+NameError: name 'sala1' is not defined
+'''},
