34b5c2994dbd73eeff14dbc1656190d2b5b9755c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-04-30 21:24:04 -0300

    Saved parisa/__log__.py @2020-05-01 00:24

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -199,3 +199,21 @@ AttributeError: 'Texto' object has no attribute 'nome'
     Texto(self.botao, "ols")
 AttributeError: 'oi' object has no attribute 'botao'
 '''},
+{'date': 'Thu Apr 30 2020 21:24:03.55 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    oi()
+  module <module> line 19
+    self.falamenino= Texto(sala1, "Muito obrigado, Senhora Neide")
+NameError: name 'sala1' is not defined
+'''},
