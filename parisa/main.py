b73efd74c20f8e3cf8dc213255f296adba99dcd4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-04-30 22:17:46 -0300

    Saved parisa/main.py @2020-05-01 01:17

--- parisa/main.py
+++ parisa/main.py
@@ -6,7 +6,7 @@ STYLE["height"] = "600px"
 #quadro negro
 BOTAO = "https://pngimage.net/wp-content/uploads/2018/05/bot%C3%A3o-power-png-7.png"
 #saladeinformatica 1 CENA
-SALA ="https://i.imgur.com/Z0jbt5J.png"
+SALA ="https://i.imgur.com/bnapnxL.png"
 TELACOMPUTER = "https://i.imgur.com/307pZY8.png"
 
 class oi:
@@ -19,6 +19,11 @@ class oi:
         self.botao.entra(self.sala1)
         self.computer=Cena(img = TELACOMPUTER)
         self.botao.vai=self.computer.vai
+        
+        self.voltar1=Elemento(img="https://image.flaticon.com/icons/png/512/74/74345.png", tit="desligar",
+        style=dict(left=80, top=510, width=80, heigth="2000000000px")) 
+        self.voltar1.entra(self.computer)
+        self.voltar1.vai=self.sala1.vai
         #self.falamenino= Texto(self.sala1, "")
         #self.botao.vai=self.falamenino.vai
        
