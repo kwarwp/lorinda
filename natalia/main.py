59b2825983d2140ccb7c1daa34bd9ddcd1a9468d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-01 00:07:24 -0300

    Saved natalia/main.py @2020-05-01 03:07

--- natalia/main.py
+++ natalia/main.py
@@ -4,3 +4,20 @@ from _spy.vitollino.main import Inventario as inv
 
 STYLE["width"] = 800
 STYLE["height"] = "600px"
+
+ZEZINHO = "https://i.imgur.com/94lhgKo.png"
+ROSALINDA = "https://i.imgur.com/qvuwHvs.png"
+CASA = "https://www.tudoconstrucao.com/wp-content/uploads/2014/12/casa-de-praia-colorida-simples.jpg"
+
+class gameg():
+
+        casa = Cena( img = Casa)
+        zezinho = Elemento( img = ZEZINHO)
+        rosalinda = Elemento( img = ROSALINDA)
+        zezinho.entra(casa)
+        rosalinda.entra(casa)
+        
+        casa.vai()
+gameg()        
+        
+        
