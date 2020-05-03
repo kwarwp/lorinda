8f01203907c039e32e96ec433d97829b9e448258
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-03 04:15:29 -0300

    Saved natalia/main.py @2020-05-03 07:15

--- natalia/main.py
+++ natalia/main.py
@@ -14,7 +14,13 @@ TOUR = "https://s2.glbimg.com/zYpJy77kEUuJR3sRh3kiTdzZ4Bk=/620x455/e.glbimg.com/
 COFRE = "https://a-static.mlcdn.com.br/618x463/cofre-concretado-com-boca-de-lobo-ct30bl-30x36x26-segredo-mecanico-cofresventura/cofresventura/5030/aeae22f74a1dac4ebacb09409c0875bd.jpg"
 HEREDOGRAMA = "https://pt-static.z-dn.net/files/d71/7bf7ab4461bea11a7a1c561ed2584a48.png"
 PERGAMIHO = "https://i.pinimg.com/originals/df/6c/76/df6c765f7019656350531c4a4eff5e11.png"
-
+BIBLIOTECA = "https://www.ifpb.edu.br/cajazeiras/noticias/2018/05/saiba-o-que-muda-com-a-implantacao-da-rede-integrada-de-bibliotecas-do-ifpb/regulamento-da-biblioteca-colegio-salesiano-dom-bosco-cidade-alta.png/@@images/image.png"
+LIVRO = "https://img2.gratispng.com/20180505/hzw/kisspng-circle-black-and-white-clip-art-5aed68ee811080.5110845515255083345287.jpg"
+SALA = "https://gizmodo.uol.com.br/wp-content/blogs.dir/8/files/2011/08/bias1-4e558ed-intro-thumb-640xauto-24932-1280x720.jpg"
+QUADRADO = "https://w1.ezcdn.com.br/rebalcomercial/fotos/grande/3250fg1/prato-raso-porcelana-26-cm-branco-quadrado-americana-germer-gmr-068.jpg"
+QUADRADO1 = "https://t2.uc.ltmcdn.com/pt/images/2/6/3/exercicio_para_calcular_a_diagonal_de_um_quadrado_24362_4_600.jpg"
+CIRCULO = "https://png.pngtree.com/png-clipart/20190223/ourlarge/pngtree-circulo-ros%C3%AA-gold-png-image_693439.jpg"
+DOIS TRAÇOS
 class gameg():
 
         casa = Cena( img = CASA)
@@ -53,7 +59,29 @@ class gameg():
         zezinho.entra(pergaminho)
         #informações sobre o heredograma
         
+        biblioteca = Cena (img = BIBLIOTECA)
+        pergaminho.direta= biblioteca
+        livro = Elemento (img = LIVRO)
+        livro.entra(biblioteca)
+        #o livro irá abrir para uma pagina secreta
         
+        sala = Cena (img = SALA)
+        quadrado = Elemento (img = QUADRADO)
+        quadrado1 = Elemento (img = QUADRADO1)
+        circulo = Elemento (img = CIRCULO)
+        dois_t = Elemento (img = DOIS_T)
+        #FAZER COM QUE O JOGADOR POSSA MONTAR ESSE HEREDROGRAMA
+        #COMADOS DE :ORGANIZE E MONTE
+        # Rapidamente após de montar tudo, apareceu alguns nomes ao lado de cada desenho :
+        #quadrado em branco - indivíduo do sexo masculino; esfera em branco - indivíduo do sexo feminino;
+        # traço - casamento; quadrado com risco no meio - indivíduo falecido 
+        # DEPOIS DE MONTAR APARECEU A PALAVRA MAPA
+        """APARECER UMA ABERTURA NA PAREDE"""
+        # EM UMA ESCRIVANINHA APARECE UM MAPA 
+        
+        
+        #APARECE UM OUTRO HEREDOGRAMA PARA ANALISAR
+        #MONTAR UM HEREDROGRAMA COM AS INDICAÇOES ABAIXO
         
         
         
