7415af4a77a09239e76fd269b710626f9198b98b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-04 02:41:58 -0300

    Saved alexa/main.py @2020-05-04 05:41

--- alexa/main.py
+++ alexa/main.py
@@ -1,2 +1,12 @@
 # lorinda.alexa.main.py
-TASSIA- 3001 isabelle
+#TASSIA- 3001 isabelle
+from _spy.vitollino.main import Cena, Elemento, Labirinto
+from _spy.vitollino.main import INVENTARIO as inv
+#1fase
+CIENTISTA="https://3.bp.blogspot.com/_YaD6P9li0m0/SOp_1mDcfPI/AAAAAAAAAHk/wGNocvYRjbY/s320/222px-Professor_Frink.png"
+ESCADA="https://i.pinimg.com/originals/77/83/aa/7783aae2cd02f662c3344824ecd43287.jpg"
+PRATELEIRA=""
+CAMERA=""
+SETAS=""
+PORTA=""
+LABORATORIO=""
