5c7b4b3b6f8915531dbd96be1b302d2ead20b10e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-04 20:06:46 -0300

    Saved parisa/__log__.py @2020-05-04 23:06

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -335,3 +335,21 @@ SyntaxError: invalid syntax
     self.falaamido= Texto(todos.leste, "Muito obrigado, Senhora Neide")
 NameError: name 'todos' is not defined
 '''},
+{'date': 'Mon May 04 2020 20:06:44.590 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 70
+    oi()
+  module <module> line 65
+    self.falamido= Texto(todos.leste, "Muito obrigado, Senhora Neide")
+NameError: name 'todos' is not defined
+'''},
