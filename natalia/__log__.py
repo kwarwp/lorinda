5d21315ca2f53269f437e6e99e9ba6a9a59e3d6e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-05 01:02:36 -0300

    Saved natalia/__log__.py @2020-05-05 04:02

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -423,3 +423,21 @@ NameError: name 'PERGAMINHO' is not defined
     pergaminho = Cena (img = PERGAMINHO)
 NameError: name 'PERGAMINHO' is not defined
 '''},
+{'date': 'Tue May 05 2020 01:02:35.213 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 193
+    gameg()        
+  module <module> line 121
+    pergaminho = Cena (img = PERGAMINHO)
+NameError: name 'PERGAMINHO' is not defined
+'''},
