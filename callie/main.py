b68e3a442f60781ace0060f918398697c432df89
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-05 19:01:29 -0300

    Saved callie/main.py @2020-05-05 22:01

--- callie/main.py
+++ callie/main.py
@@ -1,28 +1,65 @@
 # lorinda.callie.main.py
 from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto 
 from _spy.vitollino.main import inventario as inv 
-BIBLIOTECA=
-REMEDIO=
-AJUDANTE=
-LABORATORIO=
-MENSAGEM=
+DOENTE=
+CELULAR=
 LADRAO=
 CASA=
 FORMULA=
-AJUDNTE_DOENTE=
+AJUDANTED=
 CIENTISTA=
-ESCRITURAS=
+ESCRITURA=
 COMPUTADOR=
 COMPRIMIDOS=
+BIBLIOTECA=
 REMEDIO=
-GENOTIPLO1=
 PORTA=
 SALA=
-AJUDANTE_ALERGICO=
-RELOGIO=
+AJUDANTEA=
+QUADRO_DE_LUZ=
 BILHETE=
 GUARDAS=
 LIVRO=
-LANTERNA=
-REMEDIOL=
+LATERNA=
+REMEDIO_LIVRO=
+CASA_DOENTE=
+ def class boo():
+    doente= Elemento(img DOENTE)
+    celular= Elemento(img CELULAR)
+    ladrao= Elemento(img LADRAO)
+    casa= Cena(img CASA)
+    formula= Cena(img FORMULA)
+    ajudanted= Elemento(img AJUDANTED)
+    cientista= Elemento(img CIENTISTA)
+    escritura= Elemento(img ESCRITURA)
+    computador= Elemento(img COMPUTADOR)
+    comprimidos= Elementos(img COMPRIMIDOS)
+    biblioteca= Cena(img BIBLIOTECA)
+    remedio= Elemento(img REMEDIO)
+    porta= Elemento(img PORTA)
+    sala= Elemento(img SALA)
+    ajudante= Elemento(img AJUDANTEA)
+    relogio= Cena(img QUADRO_DE_LUZ)
+    bilhete= Elemento(img BILHETE)
+    guardas= Elementos(img GUARDAS)
+    livro= Elemento(img LIVRO)
+    lanterna= Elemento(img LATERNA)
+    remedio_nolivro(img REMEDIO_LIVRO)
+    casa1= Cena(img CASA_DOENTE)
+    #cena1
+    doente.entra(casa1)
+    celular.entra(casa1)
+    #doente pega o celeular
+    celular.fala"Tenho um experimento"
+    casa1.vai()
+    #cena1 parte2
+    ladrao.entra(casa)
+    formula.entra(casa)
+    #ladrao pega a formula
+    ajudanted.entra(casa)
+    doente.entra(casa)
+    doente.fala
+    
+    
+
 
