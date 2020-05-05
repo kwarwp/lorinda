ecaa0d04078061b6b0b474701862f83e1ca710ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-05 01:02:00 -0300

    Saved natalia/main.py @2020-05-05 04:02

--- natalia/main.py
+++ natalia/main.py
@@ -20,7 +20,7 @@ SALA = "https://gizmodo.uol.com.br/wp-content/blogs.dir/8/files/2011/08/bias1-4e
 QUADRADO = "https://w1.ezcdn.com.br/rebalcomercial/fotos/grande/3250fg1/prato-raso-porcelana-26-cm-branco-quadrado-americana-germer-gmr-068.jpg"
 QUADRADO1 = "https://t2.uc.ltmcdn.com/pt/images/2/6/3/exercicio_para_calcular_a_diagonal_de_um_quadrado_24362_4_600.jpg"
 CIRCULO = "https://png.pngtree.com/png-clipart/20190223/ourlarge/pngtree-circulo-ros%C3%AA-gold-png-image_693439.jpg"
-DOIS TRAÇOS
+""" 
 class gameg():
 
         casa = Cena( img = CASA)
@@ -76,7 +76,7 @@ class gameg():
         #quadrado em branco - indivíduo do sexo masculino; esfera em branco - indivíduo do sexo feminino;
         # traço - casamento; quadrado com risco no meio - indivíduo falecido 
         # DEPOIS DE MONTAR APARECEU A PALAVRA MAPA
-        """APARECER UMA ABERTURA NA PAREDE"""
+        
         # EM UMA ESCRIVANINHA APARECE UM MAPA 
         
         
@@ -87,6 +87,109 @@ class gameg():
         
         
         casa.vai()
+gameg()"""       
+        
+        
+class gameg():
+    def __init__(self):
+        self.casa = casa = Cena( img = CASA)
+        self.zezinho = Elemento( img = ZEZINHO, tit= "bOA TARDE, SOU ZEZINHO E VIM PARA A ENTREVISTA" ,
+        style=dict (left=200, top=350, width=200, height="200px",))
+        self.rosalinda = Elemento( img = ROSALINDA, tit= "OLÁ, SOU ROSALINDA,vAMOS COMEÇAR A ENTREVISTA?" ,
+        style=dict (left=400, top=350, width=300, height="200px",))
+        self.zezinho.entra(casa)
+        self.rosalinda.entra(casa)
+        self.quadros = quadros = Cena( img = QUADROS)
+        # casa.direita=quadros
+        casa.direita=Cena()
+        casa.direita.vai = self.quadros_vai
+        tour= Cena( img = TOUR)
+        tour.esquerda=quadros
+        quadros.direita=tour
+        # quadros.esquerda=casa
+        quadros.esquerda=Cena()
+        quadros.esquerda.vai = self.casa_vai
+        quadros1= Cena( img = QUADROS1)
+        tour.direita=quadros1
+        cofre = Cena(img = COFRE)# ESSE COFRE PRECISA TER UM HEREDPGRAMA PARA ABRIR
+        cofre.esquerda = quadros
+        heredo = Elemento(HEREDOGRAMA, x=540, y= 370, w=200, tit="Esse heredograma é a pista!")
+        heredo.entra(cofre)
+        o_quadro = Elemento(QUADRADO, x=340, y= 270, tit="Esse quadro tem algo diferente", style={"opacity":0.05})
+        o_quadro.vai = cofre.vai
+        o_quadro.entra(quadros)
+        pergaminho = Cena (img = PERGAMINHO)
+        quadros1.direita=pergaminho
+        pergaminho.esquerda=quadros
+        biblioteca = Cena (img = BIBLIOTECA)
+        pergaminho.direita= biblioteca
+        livro = Elemento (img = LIVRO)
+        livro.entra(biblioteca)
+        casa.vai()
+    def quadros_vai(self, *_):        
+        self.zezinho.entra(self.quadros)#colocar mesnagem confusa de zezinho em relação aos quadros
+        self.zezinho.tit = "Lindos quadros!"
+        self.rosalinda.tit = "Obrigada! Fique à vontade para examiná-los!"
+        self.rosalinda.entra(self.quadros)
+        ver_quadro = Texto(self.quadros, "ZEZINHO: Preciso examinar cada quadro, acho que tem um mistério aqui!")
+        Texto(self.quadros, "ZEZINHO: Quadros! Muitos quadros, até quadros com escrita!", foi=ver_quadro.vai).vai()
+        self.quadros.vai()
+    def casa_vai(self, *_):        
+        self.zezinho.tit = "Você é uma pessoa muito agradável!"
+        self.rosalinda.tit = "Obrigada! Procuro tratar bem as pessoas"
+        self.zezinho.entra(self.casa)
+        self.rosalinda.entra(self.casa)
+        self.casa.vai()
+    def tour(self, *_): 
+        self.zezinho.entra(tour)#zezinha volta para ver o quadros pois ficou encafifado
+        self.rosalinda.entra(tour)
+        self.rosalinda.tit = "com licença, preciso atender essa ligação...mas fique a vontade, por favor"
+        self.zezinho.tit = "preciso ver aqueles quadros novamente"
+        self.tour.vai()
+        
+    def quadros1(self, *_):
+        self.zezinho.entra(quadros1)#zezinho ajeita o quadro e quando irá arrumar outro quadro e ele está muito pesado, ao tirar da parde encontra um cofre
+        #NAO ENTENDI A GAVETA COM LETRAS
+        self.zezinho.entra.tit = "tem algo estranho com esse quadro"
+        self.quadros1.vai()
+        
+    def cofre(self, *_):
+        self.heredograma.entra(cofre)#estar como um quadro
+        self.zezinho.entra(cofre)
+        self.cofre.vai()
+        # ao clicar no heredograma ,abrir o cofre
+        #LEMBROU DE UMA AULA NA FIO CRUZ
+        #ABRIU O COFRE E PEGOU UM PAPEL
+    def pergaminho(self, *_):
+        self.zezinho.entra(pergaminho)
+        self.heredograma.entra(pergaminho)
+        self.zezinho.entra.tit = "esse heredrograma irá para algum lugar"
+        self.pergaminho.vai()
+        
+    def sala(self, *_):
+        self.quadrado.entra(sala)
+        self.quadrado1.entra(sala)
+        self.circulo.entra(sala)
+        self.zezinho.entra(sala)
+        self.zezinho.entra.tit = "acho que eu preciso fazer algo com isso"
+        self.sala.vai()
+        #dois_t = Elemento (img = DOIS_T)
+        #FAZER COM QUE O JOGADOR POSSA MONTAR ESSE HEREDROGRAMA
+        #COMADOS DE :ORGANIZE E MONTE
+        # Rapidamente após de montar tudo, apareceu alguns nomes ao lado de cada desenho :
+        #quadrado em branco - indivíduo do sexo masculino; esfera em branco - indivíduo do sexo feminino;
+        # traço - casamento; quadrado com risco no meio - indivíduo falecido 
+        # DEPOIS DE MONTAR APARECEU A PALAVRA MAPA
+        """APARECER UMA ABERTURA NA PAREDE"""
+        # EM UMA ESCRIVANINHA APARECE UM MAPA 
+        
+        
+        #APARECE UM OUTRO HEREDOGRAMA PARA ANALISAR
+        #MONTAR UM HEREDROGRAMA COM AS INDICAÇOES ABAIXO
+        
+        
+        
+        
 gameg()        
         
         
