d29c165386670491b1ac9a556a427ffaeb7fbc1e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-04 21:04:19 -0300

    Saved parisa/__log__.py @2020-05-05 00:04

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -374,3 +374,10 @@ SyntaxError: keyword argument repeated
                                                                                    ^
 SyntaxError: keyword argument repeated
 '''},
+{'date': 'Mon May 04 2020 21:04:18.333 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 41
+  self.sistema = Elemento (img= "https://i.imgur.com/n4R7Cs6.png", x =330, y=400, y=150, w=28)
+                                                                                   ^
+SyntaxError: keyword argument repeated
+'''},
