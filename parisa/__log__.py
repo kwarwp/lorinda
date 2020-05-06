22a96b781839238583dbccf550013f7ad850773f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-06 13:47:17 -0300

    Saved parisa/__log__.py @2020-05-06 16:47

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -427,3 +427,21 @@ SyntaxError: keyword argument repeated
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed May 06 2020 13:47:19.47 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 92
+    oi()
+  module <module> line 33
+    self.dois = dois = Sala (n=Sala1, s= SALA7, l= SALA8, o= SALA4)
+NameError: name 'Sala1' is not defined
+'''},
