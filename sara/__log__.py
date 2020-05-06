5a7c6856818580e6c729caf9d83ab41ed3e8e0b2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-06 20:30:36 -0300

    Saved sara/__log__.py @2020-05-06 23:30

--- sara/__log__.py
+++ sara/__log__.py
@@ -474,3 +474,21 @@ AttributeError: 'naosei' object has no attribute 'viu'
     self.foco.vai=self.falaporta.vai
 AttributeError: 'naosei' object has no attribute 'foco'
 '''},
+{'date': 'Wed May 06 2020 20:30:35.417 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 33
+    naosei()
+  module <module> line 29
+    self.foco.vai=self.falaporta.vai
+AttributeError: 'naosei' object has no attribute 'foco'
+'''},
