2476263dc52ff2c71ff4679c144c76ca6ae1ead6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-06 19:35:10 -0300

    Created sara/__log__.py @2020-05-06 22:35

---
+++ sara/__log__.py
@@ -0,0 +1,19 @@
+
+{'date': 'Wed May 06 2020 19:35:06.737 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 25
+    naosei()
+  module <module> line 23
+    auu = Elemento(FOCO, x=190, y=320, w=150, h=80, cena=self.um.oeste, style={"opacity": 40}, vai=self.frida.sul.vai)
+AttributeError: 'naosei' object has no attribute 'um'
+'''},
