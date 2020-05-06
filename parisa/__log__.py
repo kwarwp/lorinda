4be2acb7429a8824d68f9e72e561714ee417a7be
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-06 12:59:38 -0300

    Saved parisa/__log__.py @2020-05-06 15:59

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -388,3 +388,21 @@ SyntaxError: keyword argument repeated
                                                                                    ^
 SyntaxError: keyword argument repeated
 '''},
+{'date': 'Wed May 06 2020 12:59:39.936 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 80
+    oi()
+  module <module> line 24
+    self.uma= uma = Sala(o= SALA4, s= SALA5, l= SALA6, n= SALA1)
+NameError: name 'SALA5' is not defined
+'''},
