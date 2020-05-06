b940d223a72ca8afbc70d7477cbde11d9d74b887
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-06 19:44:55 -0300

    Saved sara/main.py @2020-05-06 22:44

--- sara/main.py
+++ sara/main.py
@@ -20,6 +20,6 @@ class naosei:
         
         Texto(frida.norte, "Olá, você está no centro de pesquisa. Encaminhe- se a direita ").vai()
         
-        auu = Elemento(FOCO, x=190, y=320, w=150, h=80, cena=self.um.oeste, style={"opacity": 40}, vai=self.frida.sul.vai)
+        auu = Elemento(FOCO, x=830, y=200, w=30, h=200, tit= "porta", cena=self.frida.leste, style={"opacity": 0}, vai=self.frida.sul.vai)
         Texto(frida.leste, "Vá para a sala do Dr. Zuckman").vai()
 naosei()
