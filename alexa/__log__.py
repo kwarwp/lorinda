1bc8606515701665a8b957107b51c5ef910ab49b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-06 21:49:37 -0300

    Saved alexa/__log__.py @2020-05-07 00:49

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed May 06 2020 21:49:36.706 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  cientista= lemento (img=CIENTISTA)
+  ^
+IndentationError: expected an indented block
+'''},
