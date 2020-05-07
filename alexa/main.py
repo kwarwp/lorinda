162e3303342e63e7acf30d348ca878791af3473d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-06 22:15:40 -0300

    Saved alexa/main.py @2020-05-07 01:15

--- alexa/main.py
+++ alexa/main.py
@@ -2,7 +2,7 @@
 #TASSIA- 3001 isabelle
 from _spy.vitollino.main import Cena, Elemento, Labirinto
 from _spy.vitollino.main import INVENTARIO as inv
-#fase2
+#fase1
 CIENTISTA="https://3.bp.blogspot.com/_YaD6P9li0m0/SOp_1mDcfPI/AAAAAAAAAHk/wGNocvYRjbY/s320/222px-Professor_Frink.png"
 ESCADA="https://i.pinimg.com/originals/77/83/aa/7783aae2cd02f662c3344824ecd43287.jpg"
 PRATELEIRA=""
@@ -12,17 +12,24 @@ PORTA=""
 LABORATORIO=""
 #fase2
 
-class OVINHO():
-    def __init__ (self):
+
+
+#FASE3
+
+
+
+class VINTEDOIS():
+    #def __init__(self)
     cientista= Elemento (img=CIENTISTA)
     sala1= Cena (img=ESCADA)
-    sala2=Cena (img=PRATELEIRA)
+    sala2= Cena (img=PRATELEIRA)
     sala3=Cena (img=CAMERA)
-    setas=Elemento (img= SETAS)
+    setas= Elemento (img=SETAS)
     sala4=Cena (img=PORTA)
     sala5=Cena (img=LABORATORIO)
     haha=Labirinto (norte=sala1,leste=sala2,oeste=sala3,sul=sala4)
     cientista.entra(haha.norte)
-    haha.norte.vai()
+    setas.entra(haha.sul)
+    haha.norte.vai
     
-OVINHO()
+VINTEDOIS()
