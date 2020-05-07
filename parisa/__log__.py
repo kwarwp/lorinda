d344973cbbbd58f404e97b7461ad7e05232eae00
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-07 18:59:01 -0300

    Saved parisa/__log__.py @2020-05-07 21:59

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -541,3 +541,21 @@ IndentationError: unexpected indent
     self.um.sul.direita.vai = self.texto_norte_um
 AttributeError: 'oi' object has no attribute 'texto_norte_um'
 '''},
+{'date': 'Thu May 07 2020 18:59:00.450 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 110
+    oi()
+  module <module> line 30
+    self.um.sul.direita.vai = self.texto_norte_um
+AttributeError: 'oi' object has no attribute 'texto_norte_um'
+'''},
