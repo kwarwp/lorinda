3c4e1971282b6ef58124193f8ca70d3bde5891ea
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-06 23:31:06 -0300

    Saved kathryn/main.py @2020-05-07 02:31

--- kathryn/main.py
+++ kathryn/main.py
@@ -7,27 +7,53 @@ STYLE["height"] = "600px"
 
 #CENA 1 VERBO LEMBRAR HEREDITARIEDADE
 
-SALA FIOCRUZ = "http://supygirls.pythonanywhere.com"
-ROSALINDA = "
+SALA_FIOCRUZ = "http://supygirls.pythonanywhere.com"
+ROSALINDA = "https://i.imgur.com/qvuwHvs.png"
+ZEZINHO = "https://i.imgur.com/94lhgKo.png"
 NPC = " https://i.imgur.com/NEG2o8W.jpg"
 PERGAMINHO = "https://i.imgur.com/Xe3zdhP.jpg"
 PESSOA_BRANCA_CABELO_LISO_OLHOS_VERDES = "https://i.imgur.com/6vgubFm.jpg"
-PESSOA_NEGRA_CABELO_BLACK_OLHOS_CASTANHOS = "https://i.imgur.com/wtpBiVS.jpg"
+PESSOA_NEGRA_CABELO_BLACK_OLHOS_CASTANHOS = 
 PESSOA_RUIVA_PELE_BRANCA_CABELO_VERMELHO = "https://i.imgur.com/ZcfLShe.jpg"
 CRIANCA_BRANCA_CABELO_LISO_OLHOS_VERDES = "https://i.imgur.com/ISvWiD4.jpg"
 CRIANCA_NEGRA_CABELO_BLACK_OLHOS_CASTANHOS = "https://i.imgur.com/qoGRKc8.jpg"
 CRIANCA_RUIVA_PELE_BRANCA_CABELO_VERMELHO = "https://i.imgur.com/DtSbcLh.jpg"
-MONTE_A_COMBINACAO_CORRETA = NESSE PEDACO DEVERA TER UM QUEBRA CABECA NA PAREDE ONDE O JOGADOR DEVERA REALIZAR
-O ENCAIXE CORRETO DE ACORDO COM AS NORMAS DA FEREFITARIEDADE
+QUADRO_1_PESSOA_BRANCA_CABELO_LISO_OLHOS_VERDES = "https://i.imgur.com/6vgubFm.jpg"
+QUADRO_2_PESSOA_RUIVA_PELE_BRANCA_CABELO_VERMELHO = "https://i.imgur.com/6vgubFm.jpg"
+QUADRO_3_PESSOA_RUIVA_PELE_BRANCA_CABELO_VERMELHO = "https://i.imgur.com/ZcfLShe.jpg"
+QUADRO_4_CRIANCA_BRANCA_CABELO_LISO_OLHOS_VERDES = "https://i.imgur.com/ISvWiD4.jpg"
+QUADRO_5_CRIANCA_NEGRA_CABELO_BLACK_OLHOS_CASTANHOS = "https://i.imgur.com/qoGRKc8.jpg"
+QUADRO_6_CRIANCA_RUIVA_PELE_BRANCA_CABELO_VERMELHO = "https://i.imgur.com/DtSbcLh.jpg"
+
+
+# O CÓDIGO DO MINE GAME ENTRA AQUI?
+class primeira_fase():
+
+sala_fiocruz = Cena(img= SALA_FIOCRUZ)
+rosalinda = Elemento( img = ROSALINDA, tit= "Tenho que me apresar, preciso de dinheiro para continuar minha pesquisa." ,
+        style=dict (left=400, top=350, width=300, height="200px",))
+npc = Elemento(img= NPC)
+pergaminho = Elemento(img=PERGAMINHO)
+pessoa_branca_cabelo_liso = Elemento(img= PESSOA_BRANCA_CABELO_LISO_OLHOS_VERDES)
+pessoa_negra_cabelo-black = Elemento (img = PESSOA_NEGRA_CABELO_BLACK_OLHOS_CASTANHOS)
+pessoa_ruiva_pele_branca_cabelovermelho = Elemento (img = PESSOA_RUIVA_PELE_BRANCA_CABELO_VERMELHO)
+crianca_branca-cabelo-liso_olhos_verdes = Elemento (img = CRIANCA_BRANCA_CABELO_LISO_OLHOS_VERDES)
+crianca_negra_cabelo_black_olhos_castanhos = Elemento (img = CRIANCA_NEGRA_CABELO_BLACK_OLHOS_CASTANHOS)
+crianca_ruiva_pele_branca_cabelo_vermelho = Elemento (img = CRIANCA_RUIVA_PELE_BRANCA_CABELO_VERMELHO )
+
+
 
 #CENA 2 VERBO ENTENDER HEREDITARIEDADE
 
 MUSEU_DA_VIDA = "https://i.imgur.com/s4ZBrRv.jpg"
 NPC = "https://i.imgur.com/NEG2o8W.jpg"
+rosalinda = Elemento( img = ROSALINDA, tit= "Tenho que me apresar, preciso de dinheiro para continuar minha pesquisa." ,
+        style=dict (left=400, top=350, width=300, height="200px",))
+self.textoNPC
 O NPC DEVERÁ APRESENTAR AS SEGUINTES DICAS ORGANIZAR AS PALAVRAS UTILIZANDO A ORDEM CORRETA DE ENTENDIMENTO
 
 
-DNA
+DNA = "https://www.facebook.com/Projeto-SupyGirls-2062797764002029"
 
 Genes
 
