79db2e0299d1f680a98e601098e9ad4192e1c3e0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-07 19:49:03 -0300

    Saved parisa/__log__.py @2020-05-07 22:49

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -713,3 +713,21 @@ IndentationError: expected an indented block
     self.um.oeste.vai=self.uma.sul.vai
 AttributeError: 'oi' object has no attribute 'uma'
 '''},
+{'date': 'Thu May 07 2020 19:49:02.79 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 121
+    oi()
+  module <module> line 35
+    self.todas.leste.vai=self.um.norte.vai
+AttributeError: 'oi' object has no attribute 'todas'
+'''},
