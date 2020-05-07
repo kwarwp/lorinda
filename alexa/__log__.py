b8c668ca16541cd27f89f8dadda5bf534bdc7791
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-06 21:49:21 -0300

    Created alexa/__log__.py @2020-05-07 00:49

---
+++ alexa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed May 06 2020 21:49:19.463 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  cientista=Elemento (img=CIENTISTA)
+  ^
+IndentationError: expected an indented block
+'''},
