706547d17fb9fb274f63308a33b43dc4b152c32c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-09 23:41:07 -0300

    Saved parisa/main.py @2020-05-10 02:41

--- parisa/main.py
+++ parisa/main.py
@@ -8,6 +8,9 @@ BOTAO = "https://pngimage.net/wp-content/uploads/2018/05/bot%C3%A3o-power-png-7.
 #
 FOCO = "https://i.imgur.com/6e096Va.png"
 FOCOD = "https://i.imgur.com/6e096Va.png"
+FOCOT = "https://i.imgur.com/6e096Va.png"
+FOCOQ = "https://i.imgur.com/6e096Va.png"
+FOCOC = "https://i.imgur.com/6e096Va.png"
 #saladeinformatica 1 CENA
 SALA1 ="https://i.imgur.com/bnapnxL.png"
   #teladocomputador
@@ -19,9 +22,9 @@ SALA4 = "https://i.imgur.com/fxMYKmc.png"
 # codon AUU (uau) TIROSINA
 SALA5 = "https://i.imgur.com/mLC28Jx.png"
 # codon GUA(agu) SERINA
-SALA6 = "https://i.imgur.com/AdIuakb.png"
+SALA7 = "https://i.imgur.com/AdIuakb.png"
 # codon AAU(aua) ISOLEUCINA
-SALA7 = " https://i.imgur.com/Ey3Gccw.png"
+SALA6 = " https://i.imgur.com/Ey3Gccw.png"
 #codon GAU (aug) metionina
 SALA8 = "https://i.imgur.com/9ZxVZDJ.png"
 class oi:
@@ -29,16 +32,13 @@ class oi:
     def __init__(self):
         self.um = um = Sala(n=SALA1, l=SALA2, s=SALA3, o= SALA4)
         self.um.sul.direita = Cena()
-        #self.um.sul.direita.vai = self.texto_norte_um
-
-        #self.um.oeste.direita = Cena()
-        #self.um.oeste.direita.vai = self.texto_oeste_um
+   
 
         self.uma= uma = Sala(o= SALA4, s= SALA5, l= SALA6, n= SALA1)
-        #self.uma.oeste.direita= Cena()
+      
         
         self.dois = dois = Sala (n=SALA1, s= SALA7, l= SALA8, o= SALA4)
-        #self.dois.sul.direita= Cena()
+    
 
     
       
@@ -106,9 +106,9 @@ class oi:
 
 
         auu = Elemento(FOCO, x=190, y=320, w=150, h=80, cena=self.um.oeste, style={"opacity": 0}, vai=self.texto_uma_sul)
-        aau = Elemento(FOCO, x=400, y=320, w=150, h=80, cena=self.um.oeste, style={"opacity": 0}, vai=self.dois.sul.vai)
-        gua = Elemento(FOCO, x=590, y=320, w=150, h=80, cena=self.um.oeste, style={"opacity": 0}, vai=self.uma.leste.vai)
-        gau = Elemento(FOCO, x=800, y=320, w=150, h=80, cena=self.um.oeste, style={"opacity": 0}, vai=self.dois.leste.vai)
+        aau = Elemento(FOCO, x=400, y=320, w=150, h=80, cena=self.um.oeste, style={"opacity": 0}, vai=self.texto_uma_leste)
+        gua = Elemento(FOCO, x=590, y=320, w=150, h=80, cena=self.um.oeste, style={"opacity": 0}, vai=self.texto_dois_sul)
+        gau = Elemento(FOCO, x=800, y=320, w=150, h=80, cena=self.um.oeste, style={"opacity": 0}, vai=self.texto_dois_leste)
         
         
         
@@ -118,21 +118,63 @@ class oi:
         
         Texto(self.uma.sul, "Será que esse é o nosso código genético secreto?. Clique nele e vamos desembaralhar").vai()
         self.uma.sul.vai()
+        self.falauau= Texto(self.uma.sul, "Forma correta é: UAU. Que é a Tirosina, mas infelizmente não é esse nosso código secreto. Com isso, você não conseguiu abrir o cofre. Aperte na setinha e tente novamente.")
+
+        uau = Elemento(FOCOD, x=450, y=260, w=150, h=60, cena=self.uma.sul, style={"opacity": 0}, vai=self.falauau.vai)
+       
+        self.tirosina= Elemento(img = "https://i.imgur.com/TFGte7s.png", tit= "Tirosina",
+        style=dict(left=500, top=400, width=250, heigth="100px"))
+        self.tirosina.entra(self.uma.sul)
         
-        aua = Elemento(FOCOD, x=190, y=320, w=150, h=80, cena=self.uma.sul, style={"opacity": 40})
-        self.alo= Elemento( img= "https://i.imgur.com/ZPvH4V0.jpg")
-        self.alo.entra(uma.sul)
-        #self.falaauu= Texto(self.uma.sul, "Forma correta é: AUA. Que é a Tirosina, mas infelizmente não é esse nosso código secreto. Com isso, você não conseguiu abrir o cofre. Aperte na setinha e tente novamente.")
-        #self.aua.vai=self.falaau.vai
+        self.falatiro= Texto(self.uma.sul, "A tirosina (abreviadamente Tyr ou Y)[4] ou 4-hidroxifenilalanina, É um dos 20 aminoácidos que fazem parte das proteínas. É um α-aminoácido com uma cadeia lateral formada por CH2 ligado a um grupo fenol, o que a torna uma cadeia lateral cíclica aromática com um grupo OH, que lhe confere um carácter polar neutro.")
+        self.tirosina.vai= self.falatiro.vai
+
+        
+        self.seta1=Elemento(img ="https://i.imgur.com/STflSS1.gif", tit ="voltar",
+        style=dict(left=100, top=400, width=150, heigth="30px"))
+        self.seta1.entra(self.uma.sul)
+        self.seta1.vai=self.um.oeste.vai
+    
     def texto_uma_leste(self, *_):   
         self.uma.leste.vai()
+        
         Texto(self.uma.leste, "Será que esse é o nosso código genético secreto?. Clique nele e vamos desembaralhar").vai()
-
+        self.falaaua= Texto(self.uma.leste, "Forma correta é: AUA. Que é a Isoleucina, mas infelizmente não é esse nosso código secreto. Com isso, você não conseguiu abrir o cofre. Aperte na setinha e tente novamente.")
+        aua = Elemento(FOCOT, x=450, y=260, w=150, h=60, cena=self.uma.leste, style={"opacity": 0}, vai=self.falaaua.vai)
+        
+        self.isoleucina=Elemento(img = "https://i.imgur.com/tL2cc9H.png", tit = "Isoleucina",
+        style=dict(left=500, top=400, width=150, heigth="80px"))
+        self.isoleucina.entra(self.uma.leste)
+        
+        self.seta2=Elemento(img ="https://i.imgur.com/STflSS1.gif", tit ="voltar",
+        style=dict(left=100, top=400, width=150, heigth="30px"))
+        self.seta2.entra(self.uma.leste)
+        self.seta2.vai=self.um.oeste.vai
+    
+        self.falaiso= Texto(self.uma.leste, "A Isoleucina é utilizada pelo organismo especialmente para construir os tecidos musculares. A isoleucina, leucina e valina são os aminoácidos de cadeia ramificada e são melhor absorvidos e utilizados pelo organismo na presença de vitaminas do complexo B, como o feijão ou lecitina de soja.")
+        self.isoleucina.vai=falaiso.vai
+    
     def texto_dois_sul(self, *_):    
         self.dois.sul.vai()
         Texto(self.dois.sul, "Será que esse é o nosso código genético secreto?. Clique nele e vamos desembaralhar").vai()
+        self.falaagu= Texto(self.dois.sul, "Forma correta é: AGU. Que é a Serina, mas infelizmente não é esse nosso código secreto. Com isso, você não conseguiu abrir o cofre. Aperte na setinha e tente novamente.")
+        agu = Elemento(FOCOT, x=450, y=260, w=150, h=60, cena=self.dois.sul, style={"opacity": 0}, vai=self.falaagu.vai)
+        self.serina=Elemento(img= "https://i.imgur.com/IDBTwzj.png", tit= "Serina",
+        style=dict(left=530, top=400, width=150, heigth="80px"))
+        self.serina.entra(self.dois.sul)
+        
+        self.seta3=Elemento(img ="https://i.imgur.com/STflSS1.gif", tit ="voltar",
+        style=dict(left=100, top=400, width=150, heigth="30px"))
+        self.seta3.entra(self.dois.sul)
+        self.seta3.vai=self.um.oeste.vai
+        
+        self.falaseri=Texto(self.dois.sul, "A serina é importante no metabolismo, dado que participa na biossíntese de purinas e pirimidinas. É o percursor de vários aminoácidos, incluindo glicina e cisteína, e triptofano em bactérias.")
+        self.serina.vai=self.falaseri
+        
     def texto_dois_leste(self, *_):  
         self.dois.leste.vai()
         Texto(self.dois.leste, "Será que esse é o nosso código genético secreto?. Clique nele e vamos desembaralhar").vai()
+        self.falaaug= Texto(self.dois.leste, "Forma correta é: AUA. Que é a Isoleucina, mas infelizmente não é esse nosso código secreto. Com isso, você não conseguiu abrir o cofre. Aperte na setinha e tente novamente.")
+        aug = Elemento(FOCOT, x=450, y=260, w=150, h=60, cena=self.dois.leste, style={"opacity": 0}, vai=self.falaaug.vai)
         
 oi()
