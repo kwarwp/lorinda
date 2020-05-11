723c44e10b76ec2fb9e0b20897f443d8b7c3cd1f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-10 21:22:00 -0300

    Saved parisa/__log__.py @2020-05-11 00:21

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -827,3 +827,21 @@ NameError: name 'Elemeneto' is not defined
     pasta= Elemento(FICOSE, x=660, y=250, w=130, h=80, cena=self.um.sul, style={"opacity": 0}, vai=self.um.oeste.vai)
 NameError: name 'FICOSE' is not defined
 '''},
+{'date': 'Sun May 10 2020 21:21:58.960 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 181
+    oi()
+  module <module> line 67
+    pasta= Elemento(FICOSE, x=660, y=250, w=130, h=80, cena=self.um.sul, style={"opacity": 0}, vai=self.um.oeste.vai)
+NameError: name 'FICOSE' is not defined
+'''},
