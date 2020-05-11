2787aec7b7bd196d7f7d49271b825e27b2d5c0ca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-11 17:44:42 -0300

    Saved parisa/__log__.py @2020-05-11 20:44

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -888,3 +888,21 @@ NameError: name 'FICOSE' is not defined
                                                                                                                                                                                                                                                                             ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon May 11 2020 17:44:40.359 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 209
+    oi()
+  module <module> line 47
+    self.tres = tres (o=SALA4, s= SALA9, l=SALA10, n=SALA1)
+NameError: name 'tres' is not defined
+'''},
