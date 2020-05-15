89e2195a9403f1e5e57b1194979a7fe4765aff10
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-05-15 16:48:12 -0300

    Saved natalia/__log__.py @2020-05-15 19:48

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -821,3 +821,21 @@ UnboundLocalError: local variable 'quartos2' referenced before assignment
     sala.direita=quartos2
 UnboundLocalError: local variable 'quartos2' referenced before assignment
 '''},
+{'date': 'Fri May 15 2020 16:48:11.470 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 434
+    gameg()        
+  module <module> line 131
+    sala.direita=quartos2
+UnboundLocalError: local variable 'quartos2' referenced before assignment
+'''},
