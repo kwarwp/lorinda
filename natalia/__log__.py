5f3de23f4ad4c9b974ba264acaa052ff34d987e6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-06-02 22:50:50 -0300

    Saved natalia/__log__.py @2020-06-03 01:50

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -839,3 +839,21 @@ UnboundLocalError: local variable 'quartos2' referenced before assignment
     sala.direita=quartos2
 UnboundLocalError: local variable 'quartos2' referenced before assignment
 '''},
+{'date': 'Tue Jun 02 2020 22:50:49.150 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 275
+    class aah :
+  module <module> line 276
+    def __init__(self, esta_cena, chama_quando_acerta, partes=(ii2, iih3, iih, iih1)):# COM IMAGEM
+NameError: name 'ii2' is not defined
+'''},
