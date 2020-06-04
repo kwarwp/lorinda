302229b88e453a60c34848963e1b15635d8f9872
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-06-03 21:07:02 -0300

    Saved natalia/__log__.py @2020-06-04 00:07

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -857,3 +857,21 @@ UnboundLocalError: local variable 'quartos2' referenced before assignment
     def __init__(self, esta_cena, chama_quando_acerta, partes=(ii2, iih3, iih, iih1)):# COM IMAGEM
 NameError: name 'ii2' is not defined
 '''},
+{'date': 'Wed Jun 03 2020 21:07:01.929 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 275
+    class aah :
+  module <module> line 276
+    def __init__(self, esta_cena, chama_quando_acerta, partes=(ii2, iih3, iih, iih1)):# COM IMAGEM
+NameError: name 'ii2' is not defined
+'''},
