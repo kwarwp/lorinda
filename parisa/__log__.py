dc2940aee0837e3f29028c9a2b78e354ce95353f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-24 11:17:54 -0300

    Saved parisa/__log__.py @2020-07-24 14:17

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -913,3 +913,21 @@ NameError: name 'tres' is not defined
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Fri Jul 24 2020 11:17:56.577 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 38
+    class oi:
+  module <module> line 225
+    self.tres.leste.vai()
+AttributeError: 'GUI' object has no attribute 'tres'
+'''},
