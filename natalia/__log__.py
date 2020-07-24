dfb3eaafcd5cc04c6ac2b6966c83fdbe7424b01b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-24 16:07:32 -0300

    Saved natalia/__log__.py @2020-07-24 19:07

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -915,3 +915,21 @@ NameError: name 'ff5' is not defined
     quadros1.direita.vai=abrepergaminho
 NameError: name 'abrepergaminho' is not defined
 '''},
+{'date': 'Fri Jul 24 2020 16:07:32.89 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 456
+    gameg()        
+  module <module> line 147
+    quadros1.direita.vai=abrepergaminho
+NameError: name 'abrepergaminho' is not defined
+'''},
