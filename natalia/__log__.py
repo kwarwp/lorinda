caff51668d65878a05980c233278e6ff6b24f7fc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-24 16:06:50 -0300

    Saved natalia/__log__.py @2020-07-24 19:06

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -897,3 +897,21 @@ NameError: name 'ii2' is not defined
     def __init__(self, esta_cena, chama_quando_acerta, partes=(ff5, ff3, ff2, ff, ff1, ff4)):#COM IMAGEM 
 NameError: name 'ff5' is not defined
 '''},
+{'date': 'Fri Jul 24 2020 16:06:49.870 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 456
+    gameg()        
+  module <module> line 147
+    quadros1.direita.vai=abrepergaminho
+NameError: name 'abrepergaminho' is not defined
+'''},
