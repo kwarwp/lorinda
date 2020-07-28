bfc587c234edf3e3e57f372aaebe6c5edde89416
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-28 16:38:43 -0300

    Saved meredith/main.py @2020-07-28 19:38

--- meredith/main.py
+++ meredith/main.py
@@ -1,4 +1,19 @@
 # lorinda.meredith.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto
 from _spy.vitollino.main import inventario as inv
-Box =
+BOX=
+BONECA=
+URSO=
+BONECO=
+CASTELO=
+PRAIA=
+RUINAS=
+BOLA=
+TELEFONE=
+CENARION=
+CENARIOD=
+PORTAO=
+PORTA=
+QUARTODAPRICESA=
+QUARTODOPRINCIPE=
+REDEDEVOLEI=
