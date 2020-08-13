98aec29f7d7ed5bf80e2f707ef438aa72c7547a5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-08-12 21:36:27 -0300

    Saved natalia/__log__.py @2020-08-13 00:36

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -976,3 +976,25 @@ NameError: name 'quadro1' is not defined
                                                                                                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 12 2020 21:36:26.405 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 490
+    gameg()        
+  module <module> line 160
+    HerdogramaBiblioteca(biblioteca,self.mostra_conteudo_biblioteca)
+  module <module> line 388
+    self.linhas = [
+  module <module> line 372
+    self.linha = Elemento(self.image, x=posicao*100, y=20, w=90, h=70,
+AttributeError: 'LinhaGeracional' object has no attribute 'image'
+'''},
