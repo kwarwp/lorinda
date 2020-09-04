b07cca7accc6aa9da30d4a0ffa3c971a25ed6cc8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-03 21:57:48 -0300

    Created lisa/main.py @2020-09-04 00:57

---
+++ lisa/main.py
@@ -0,0 +1,11 @@
+# lorinda.lisa.main.py
+from _spy.vitollino.main import Cena, Elemento, Style, Sala
+from _spy.vitollino.main import inventario as inv
+
+GRUPO.ESTUDANTES=""
+DRA.ROSALIND=""
+EQUIPAMENTO="'
+LABORATORIO="'
+
+
+alunos= Elemento(GRUPO.ESTUDANTES)
