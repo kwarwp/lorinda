1bcb08e6103a25b122de8d5637d8e9a71f193e5a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-07 16:44:10 -0300

    Saved lisa/__log__.py @2020-09-07 19:44

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -107,3 +107,21 @@ NameError: name 'GRUPO' is not defined
     GRUPO.ESTUDANTES="https://static.lenovo.com/br/landings/estudantes/assets/img/lenovo-educational-banner.png"
 NameError: name 'GRUPO' is not defined
 '''},
+{'date': 'Mon Sep 07 2020 16:44:08.965 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 177
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 10
+    class celulas:
+  module <module> line 19
+    fala.vai()
+NameError: name 'fala' is not defined
+'''},
