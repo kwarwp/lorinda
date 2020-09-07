71882a87cb13103748f171f750301671fc2552ae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-07 16:38:52 -0300

    Saved lisa/__log__.py @2020-09-07 19:38

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -6,3 +6,10 @@
                 ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Mon Sep 07 2020 16:38:51.253 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  alunos= Elemento(GRUPO.ESTUDANTES)
+  ^
+IndentationError: expected an indented block
+'''},
