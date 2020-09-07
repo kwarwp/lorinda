a393c50d1c4fb219939270538bfbe02ec8f22001
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-07 16:44:18 -0300

    Saved lisa/main.py @2020-09-07 19:44

--- lisa/main.py
+++ lisa/main.py
@@ -1,20 +1,20 @@
 # lorinda.lisa.main.py
-from _spy.vitollino.main import Cena, Elemento, Style, Sala
-from _spy.vitollino.main import inventario as inv
+from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
+from _spy.vittolino.main import INVENTARIO as inv
 
-GRUPO.ESTUDANTES=""
-DRA.ROSALIND=""
-EQUIPAMENTO="'
-LABORATORIO="'
+GRUPO_ESTUDANTES="https://static.lenovo.com/br/landings/estudantes/assets/img/lenovo-educational-banner.png"
+DRA_ROSALIND="https://blog.ipog.edu.br/wp-content/uploads/2017/10/m%C3%A9dico.jpg"
+EQUIPAMENTO="https://comps.canstockphoto.com.br/equipamento-m%C3%A9dico-desenho-vetor-eps_csp55846331.jpg"
+LABORATORIO="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/InvestigadoresUR.JPG/350px-InvestigadoresUR.JPG"
 
-class celulas():
-alunos= Elemento(GRUPO.ESTUDANTES)
-rosalind= Elemento(DRA.ROSALIND)
-maquina= Elemento(EQUIPAMENTO)
-laboratorio= Cena(LABORATORIO)
+class celulas:
+    alunos= Elemento(GRUPO_ESTUDANTES)
+    rosalind= Elemento(DRA_ROSALIND)
+    maquina= Elemento(EQUIPAMENTO)
+    laboratorio= Cena(LABORATORIO)
     rosalind.entra(laboratorio)
     alunos.entra(laboratorio)
     laboratorio.vai()
     rosalind.fala="Olá, me chamo Rosalind. Hoje acompanharei vocês por este passeio pelas células!"
     fala.vai()
-     
+celulas()   
