64ab347b6a0e727b85b408834aeb949a6ee85116
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-07 16:34:22 -0300

    Created lisa/__log__.py @2020-09-07 19:34

---
+++ lisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Sep 07 2020 16:34:20.353 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  EQUIPAMENTO="'
+                ^
+SyntaxError: EOL while scanning string literal
+'''},
