caec9ea8a0f3b8c2d1d2f2bdbf10848c9d128133
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-08 16:09:15 -0300

    Saved lisa/main.py @2020-09-08 19:09

--- lisa/main.py
+++ lisa/main.py
@@ -1,5 +1,5 @@
 # lorinda.lisa.main.py
-from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
+from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE, Texto
 from _spy.vittolino.main import INVENTARIO as inv
 STYLE["width"] = 1150
 STYLE["height"] = "550px"
@@ -8,14 +8,16 @@ DRA_ROSALIND="https://blog.ipog.edu.br/wp-content/uploads/2017/10/m%C3%A9dico.jp
 EQUIPAMENTO="https://comps.canstockphoto.com.br/equipamento-m%C3%A9dico-desenho-vetor-eps_csp55846331.jpg"
 LABORATORIO="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/InvestigadoresUR.JPG/350px-InvestigadoresUR.JPG"
 
-class celulas:
+class celulas():
     alunos= Elemento(GRUPO_ESTUDANTES,  x=460, y=250, w=150, h=80)
-    rosalind= Elemento(DRA_ROSALIND,  x=400, y=250, w=150, h=80)
+    rosalind= Elemento(DRA_ROSALIND,  x=400, y=250, w=150, h=80, Texto " Aqui é um castelo da ciênca onde vcs poderam aprender, descobrir e inventar")
     maquina= Elemento(EQUIPAMENTO,  x=360, y=250, w=150, h=80)
-    laboratorio= Cena(LABORATORIO)
+    laboratorio= Cena(LABORATORIO, Texto " Bem vindo ao laborátorio de ciências, aqui aprendemos e estudamos células")
     rosalind.entra(laboratorio,)
     alunos.entra(laboratorio)
     laboratorio.vai()
-    rosalind.fala="Olá, me chamo Rosalind. Hoje acompanharei vocês por este passeio pelas células!"
+    rosalind.fala="Olá, me chamo Rosalind. Hoje acompanharei vocês por este passeio pelas células!"# ver se não vai chocar com o que eu colequei nesse personagem acima 
     fala.vai()
-celulas()   
+celulas()
+
+class Fase1():
