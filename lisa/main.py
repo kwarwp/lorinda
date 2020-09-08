6009cd8e88734719bca20fe16ac471be5dd56f84
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-08 18:01:33 -0300

    Saved lisa/main.py @2020-09-08 21:01

--- lisa/main.py
+++ lisa/main.py
@@ -24,13 +24,14 @@ class Fase1():
 laboratorio= Cena(img =  LABORATORIO)
 procarionte= Elemento(img= PROCARIONTE, Texto= " oi eu sou uma célula procarionte,e me chama assim pq eu tenho apenas uma célula no meu corpo")
 eucarionte= Elemento(img= EUCARIONTE, Texto= "eu sou uma célula procarionte e sou igual a vcs humanos pq meu corpo é cheio de células")
-trypanosoma_cruzi= Elemento(img=TRYPANOSOMA_CRUZI )
-neuronio=Elemento(img= NEURONIO, Texto ="eus ou bem complicado, pois eu fico na cabeça e passo informação pelo corpo, imagina como eu devo ser feito))
+trypanosoma_cruzi= Elemento(img=TRYPANOSOMA_CRUZI, Texto = "a galera não gosta muito de mim pq dizem que eu sou parasita e que causa uma doença no coração, mas é que o coração grande é tão quentinho"
+)
+neuronio=Elemento(img= NEURONIO, Texto ="eus ou bem complicado, pois eu fico na cabeça e passo informação pelo corpo, imagina como eu devo ser feito")
 bacteria = Elemento(img= BACTERIA, Texto ="olá eu sou uma pessoa simples, igual arroz pois combino com tudo e posso estar em qualquer lugar, sempre tem espaço para mim")
-espermatozoide= Elemento(img=  ESPERMATOZOIDE, Texto = "eu sou quem dá a origem dos humanos, então imagina como devo ser dificil e complexo me estudar")) 
+espermatozoide= Elemento(img=  ESPERMATOZOIDE, Texto = "eu sou quem dá a origem dos humanos, então imagina como devo ser dificil e complexo me estudar") 
 hemacia= Elemento(img= HEMACIA, Texto= "eu sou o transporte da galera, carrego muita coisa, imagina como sou")
-elodea= Elemento(img= ELODEA)
-celula_calciforme= Elemento(img= CELULA_CALCIFORME
+elodea= Elemento(img= ELODEA, Texto ="eu vivo na agua e sou muito importante para a manutenção do ambiente marinho, posso ser encoderijo , comida ou o2")
+celula_calciforme= Elemento(img= CELULA_CALCIFORME,Texto = "eu fico no instestino delgado eu tenho diversas funções, uma dela pe revestir com muco para proteger onde estou")
 npc= Elemento(img= NPC,Texto = "você precisa descobrir quem é procarionte para a próxima fase")
 """resposta certa é a bactéria"""
 laboratorio.vai()
