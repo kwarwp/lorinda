d80fcd47ffa059fa8a629637e0af51407f3157fa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-15 03:10:37 -0300

    Saved lisa/__log__.py @2020-09-15 06:10

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -265,3 +265,10 @@ SyntaxError: invalid syntax
                              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Sep 15 2020 03:10:36.134 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 44
+  maria.entra(fiocruz, Texto"acho que eu deveria jogar o pacote fora,mas se eu olhar antes e depois jogar?")
+                             ^
+SyntaxError: invalid syntax
+'''},
