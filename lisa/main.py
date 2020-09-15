0c52e3bca5e345d844ed0fbcf487a4f95c0c2f0c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-15 19:34:31 -0300

    Saved lisa/main.py @2020-09-15 22:34

--- lisa/main.py
+++ lisa/main.py
@@ -21,20 +21,20 @@ LABORATORIO ="https://www.unimedjaboticabal.coop.br/wp-content/uploads/2019/07/u
 #colocar imagens da class apresentação 
 class apresentacao():
     casa = Cena(img = CASA)#maria vai para fio cruz
-    maria= Elemento(img= MARIA, Texto= " olá me chamo Maria e amo estudar biologia e lutar pelos direitos das mulheres", x=460, y=250, w=150, h=80)
+    maria= Elemento(img= MARIA, texto= " olá me chamo Maria e amo estudar biologia e lutar pelos direitos das mulheres",cena=a_cena, x=460, y=250, w=150, h=80)
     rua= Cena(img= RUA)
-    estranha= Elemento(img= ESTRANHA)
+    estranha= Elemento(img= ESTRANHA, texto= "Guarde e proteja esse pacote com sua própria vida")
     pacote=Elemento(img=PACOTE)
     casa.direta=rua
     rua.esquerda=casa
     maria.entra(rua)
     #coloquei .vai() em alguns
-    maria.entra(rua, Texto= "estou indo para FIOCRUZ, acho que vou conseguir um estágio lá, uip").vai()
+    maria.entra(rua, texto= "estou indo para FIOCRUZ, acho que vou conseguir um estágio lá, uip").vai()
     rua2=Cena(img= RUA2)
     rua.direita= rua2
     rua2.esquerda=rua
-    maria.entra(rua2, Texto= "parece uma pessoa estranha, não quero aceitar nada dela").vai()
-    estranha.entra(rua2, Texto= "Guarde e proteja esse pacote com sua própria vida")#entrega o pacote e some
+    maria.entra(rua2, texto= "parece uma pessoa estranha, não quero aceitar nada dela").vai()
+    estranha.entra(rua2)#entrega o pacote e some
     pacote=Elemento(img= PACOTE)
     pacote.entra(rua2)
     rua2.direta=fiocruz
@@ -45,22 +45,23 @@ class apresentacao():
     #será que assim vai? colocar separado? mas se colocar assim os dois textos vão aparecer
     #juntos, não?
     Texto(fiocruz, "Boa tarde Maria, temos um tempo antes da entrevista, fique a vontade...se quiser pode explorar os laboatórios para estudar").vai()
-    maria.entra(fiocruz, Texto="acho que eu deveria jogar o pacote fora,mas se eu olhar antes e depois jogar?")
+    maria.entra(fiocruz, texto="acho que eu deveria jogar o pacote fora,mas se eu olhar antes e depois jogar?")
     
     #maria abre o pacote e encontra uma lâmina
-    lamina=Elemento(img= LAMINA, Texto= "nossa um pedaço de vidro, tenho um tempo antes da entrevista do estagio a moça disse que eu poderia ficar nos laboratórios estudando")
+    lamina=Elemento(img= LAMINA, texto= "nossa um pedaço de vidro, tenho um tempo antes da entrevista do estagio a moça disse que eu poderia ficar nos laboratórios estudando")
     #maria vai para um laboratório, mas antes passa por um corredor
     corredor= Cena(img= CORREDOR)
     fiocruz.direita= corredor
     corredor.esquerda=fiocruz
-    laboratorio= Cena(img= LABORATORIO, Texto = "aqui tem uns equipamentos para ver esse vidro, escolha qual vc acha melhor")
-    microscopio = Elemento(img = microscopio, Texto = " olá, eu sou utilizado para ver coisas que o olho humano não ve ")
+    laboratorio= Cena(img= LABORATORIO, texto = "aqui tem uns equipamentos para ver esse vidro, escolha qual vc acha melhor")
+    microscopio = Elemento(img = microscopio, texto = " olá, eu sou utilizado para ver coisas que o olho humano não ve ")
 
     """ colocar outros equipamentos de laboratório"""
     cartaz= Elemento(img= CARTAZ, Texto = "para se usar o microscópio faça isso...")
     """arrastar e colocar a lâmina no microscópio e então aparece um um ciclone que a leva para dentro da célula, 
     ela grita e pergunta onde estou""" 
     
+
 apresentacao()
 print (apresentacao)
 
