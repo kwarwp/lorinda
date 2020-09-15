83649549e41e00a3321ddc0e4e2b2c98ab3e213f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-15 03:18:23 -0300

    Saved lisa/__log__.py @2020-09-15 06:18

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -349,3 +349,10 @@ SyntaxError: invalid syntax
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Sep 15 2020 03:18:21.711 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 80
+  """
+  ^
+IndentationError: expected an indented block
+'''},
