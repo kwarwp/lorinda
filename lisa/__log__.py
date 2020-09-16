74a92f1c5cf96b3ca80773c9bc46249c5ac93fb5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-16 00:18:27 -0300

    Saved lisa/__log__.py @2020-09-16 03:18

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -405,3 +405,25 @@ NameError: name 'CARTAZ' is not defined
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Sep 16 2020 00:18:26.540 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 73
+    apresentacao()
+  module <module> line 25
+    maria= Elemento(img= MARIA, texto= " olá me chamo Maria e amo estudar biologia e lutar pelos direitos das mulheres",cena=a_cena, x=460, y=250, w=150, h=80)
+NameError: name 'a_cena' is not defined
+'''},
