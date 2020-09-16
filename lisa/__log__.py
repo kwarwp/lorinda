e6819080a8e7d6338f45d695c3be14eca0d64e51
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-16 00:22:12 -0300

    Saved lisa/__log__.py @2020-09-16 03:22

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -456,3 +456,25 @@ SyntaxError: invalid syntax
     maria= Elemento(img= MARIA,cena=a_cena, x=460, y=250, w=150, h=80)
 NameError: name 'a_cena' is not defined
 '''},
+{'date': 'Wed Sep 16 2020 00:22:10.684 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 76
+    apresentacao()
+  module <module> line 33
+    maria.entra(rua, texto= "estou indo para FIOCRUZ, acho que vou conseguir um estágio lá, uip")
+TypeError: entra() got an unexpected keyword argument 'texto'
+'''},
