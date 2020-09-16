5ef1837dd2aebadf5b9551549faf3fe99f00165c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-16 00:15:49 -0300

    Saved lisa/__log__.py @2020-09-16 03:15

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -380,3 +380,21 @@ Exception: <TypeError: innermost.C.tree[0].args.indexOf is not a function>
 'error': '''
 Exception: <TypeError: innermost.C.tree[0].args.indexOf is not a function>
 '''},
+{'date': 'Wed Sep 16 2020 00:15:48.304 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 22
+    class apresentacao():
+  module <module> line 65
+    cartaz= Elemento(img= CARTAZ, Texto = "para se usar o microscópio faça isso...")
+NameError: name 'CARTAZ' is not defined
+'''},
