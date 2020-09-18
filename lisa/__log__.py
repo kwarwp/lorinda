8a1fcce59af8d5c5e7bff1b0c00217668b3c807d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-18 18:59:51 -0300

    Saved lisa/__log__.py @2020-09-18 21:59

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -718,3 +718,20 @@ Traceback (most recent call last):
     laboratorio.vai()
 NameError: name 'laboratorio' is not defined
 '''},
+{'date': 'Fri Sep 18 2020 18:59:52.737 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''<class 'apresentacao'>
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 134
+    laboratorio.vai()
+NameError: name 'laboratorio' is not defined
+'''},
