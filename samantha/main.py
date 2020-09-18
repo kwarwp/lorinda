71d4e3c66f105ce05e39039055e75df196fd25d6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-18 20:54:26 -0300

    Created samantha/main.py @2020-09-18 23:54

---
+++ samantha/main.py
@@ -0,0 +1,47 @@
+# lorinda.samantha.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto, Sala
+from _spy.vitollino.main import inventario as inv
+STYLE=
+STYLE=
+
+FLAVINHO =""
+FOTO_PRINCIPAL =""
+DALMACIA =""
+JERONIMO_JOVEM =""
+JERONIMO_VELHO =""
+CURIOSIDADE ="" 
+LEAO =""
+PAPA_LIBERIO=""
+BATIZADO_JERONIMO  =""
+ROMA =""
+ORDDENACAO_SACERDOTAL =""
+SONHO =""
+CURIOSIDADE =""
+JERONIMO_CAVERNA =""
+BIBLIA =""
+VULGATA =""
+BELEM =""
+FOTO_DO_PADROEIRO =""
+FRASE =""
+CENA_FINAL=""
+ class kkkk():
+    leao= Cena(img=LEAO)
+    falvinho= Elemento(img=FALVINHO)
+    f_t= Cena(img=FOTO_PRINCIPAL)
+    dalmacia= Cena(img=DALMACIA)
+    j_j= Elemento(img=JERONIMO_JOVEM)
+    j_v= Elemento(img=JERONIMO_VELHO)
+    batizado= Cena(img=BATIZADO_JERONIMO)
+    roma= Cena(img=ROMA)
+    ordenacao= Cena(img=ORDDENACAO_SACERDOTAL)
+    sonho= Cena(img=SONHO)
+    cr= Cena(img=CURIOSIDADE)
+    pd= Cena(img=CURIOSIDADE)
+    pl= Elemento(img=PAPA_LIBERIO)
+    j_c= Cena(img=JERONIMO_CAVERNA)
+    biblia= Cena(img=BIBLIA)
+    vulgata= Cena(img=VULGATA)
+    belem= Cena(img=BELEM)
+    f_p= Cena(img=FOTO_DO_PADROEIRO)
+    frase= Cena(img=FRASE)
+    cf= Cena(img=CENA_FINAL)
