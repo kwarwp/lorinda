b61a8ecd2228ed5a6d0118cd752df6391bc1ef71
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-20 20:18:38 -0300

    Saved samantha/main.py @2020-09-20 23:18

--- samantha/main.py
+++ samantha/main.py
@@ -24,9 +24,13 @@ BELEM =""
 FOTO_DO_PADROEIRO =""
 FRASE =""
 CENA_FINAL=""
+PAPAD=""
+CAVERNA=""
+FOTO_PAROQUIA=""
  class kkkk():
+    ft= Cena(img=FOTO_PAROQUIA)
     leao= Cena(img=LEAO)
-    falvinho= Elemento(img=FALVINHO)
+    padre= Elemento(img=FALVINHO)
     f_t= Cena(img=FOTO_PRINCIPAL)
     dalmacia= Cena(img=DALMACIA)
     j_j= Elemento(img=JERONIMO_JOVEM)
@@ -36,7 +40,7 @@ CENA_FINAL=""
     ordenacao= Cena(img=ORDDENACAO_SACERDOTAL)
     sonho= Cena(img=SONHO)
     cr= Cena(img=CURIOSIDADE)
-    pd= Cena(img=CURIOSIDADE)
+    caverna= Cena(img=CAVERNA)
     pl= Elemento(img=PAPA_LIBERIO)
     j_c= Cena(img=JERONIMO_CAVERNA)
     biblia= Cena(img=BIBLIA)
@@ -45,9 +49,48 @@ CENA_FINAL=""
     f_p= Cena(img=FOTO_DO_PADROEIRO)
     frase= Cena(img=FRASE)
     cf= Cena(img=CENA_FINAL)
+    pd= Elemento(img=PAPAD)
     
     def entrou_padre(*_):
-        entrou.padre(capela)
-        padre.vai=Texto(capela, "olá pessoal, tudo certinho?")
-        padre.vai=Texto(capela, "Eu sou o Padre Flávio e vou te ajudar nessa aventura sobre a história do nosso amado padroeiro")
+        entrou.padre(f_t)
+        padre.vai=Texto(f_t, "olá pessoal, certinho?")
+        padre.vai=Texto(f_t, "Eu sou o Padre Flávio e vou te ajudar nessa aventura sobre a história do nosso amado padroeiro")
+        
+    def entrou_1():
+        entrou.padre(dalmacia)
+        padre.vai=Texto(dalmacia, "São Jerônimo nasceu na Dalmácia no ano de 340.")
+        
+        
+    def entrou_2():
+        entrou.padre(roma)
+        padre.vai=Texto(roma, "Após a morte de seus pais, Jerônimo foi para Roma estudar e durante sua permanencia teve um sonho muito importante para sua conversão.")
+        
+        
+    def entrou_3
+        entrou.padre(sonho)
+        padre.vai=Texto(sonho, "No sonho, Jerônimo apresentava-se como cristão e era repreedindo pelo próprio Cristo por estar faltando com a verdade.")
+        
+
+    def entrou_4
+        entrou.padre(pl)
+        padre.vai=Texto(pl, "Aos 25 anos de idade Jerônimo foi batizado pelo Papa Libério no fim de sua permanencia em Roma.")
+        entrou.padre(fp)
+        padre.vai=Texto(fp, "Aos 25 anos de idade Jerônimo foi batizado pelo Papa Libério no fim de sua permanencia em Roma.")
+        
+    def entrou_5
+        entrou.padre(ordenacao)
+        padre.vai=Texto(ordenacao, "Jerônimo foi ordenado sacerdote no ano de 379, retirando-se para dedicar-se ao estudo.")
+        
+    
+    def entrou_curi
+        entrou.padre(curiosidade)
+        padre.vai=Texto(curiosidade, "Jerônimo foi ordenado sacerdote no ano de 379, retirando-se para dedicar-se ao estudo.")
+         
+    def entrou_6
+        entrou.pd(caverna)
+        entrou.padre(caverna)
+        padre.vai=Texto(caverna,"-Por ter aprendido as linguas originais para melhor compreender as escrituras, Nosso padroeiro pôde a pedido do Papa Damaso traduzir as escrituras para o Latim.")
+    
+    
+    def entro_7
         
