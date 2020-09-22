5792e2e5e1047f832a2acf08863c58c0bc0a01f8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-22 18:19:56 -0300

    Saved lisa/__log__.py @2020-09-22 21:19

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -1047,3 +1047,21 @@ AttributeError: 'apresentacao' object has no attribute 'microscopio'
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Sep 22 2020 18:19:53.293 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 115
+    apresentacao().vai()
+  module <module> line 70
+    self.casa.texto.foi=self.entrou_maria
+AttributeError: 'Cena' object has no attribute 'texto'
+'''},
