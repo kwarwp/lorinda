509ff22e6cee5f2db97e744a9d326fe5d53b2cb1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 21:32:40 -0300

    Saved lisa/__log__.py @2020-09-22 00:32

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -855,3 +855,21 @@ NameError: name 'laboratorio' is not defined
     celula= Cena(img= CELULA)#QUANDO CLICA NO MICRÓSCÓPIO IRÁ PARA ESSA CENA
 NameError: name 'CELULA' is not defined
 '''},
+{'date': 'Mon Sep 21 2020 21:32:38.302 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 96
+    apresentacao()
+  module <module> line 60
+    celula= Cena(img= CELULA)#QUANDO CLICA NO MICRÓSCÓPIO IRÁ PARA ESSA CENA
+NameError: name 'CELULA' is not defined
+'''},
