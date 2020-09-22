1a6f3fc6c1ea916222d894fa8e8e36e44dc969f9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-22 09:48:35 -0300

    Saved lisa/__log__.py @2020-09-22 12:48

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -926,3 +926,20 @@ SyntaxError: keyword can't be an expression
     celula= Cena(img= CELULA)#QUANDO CLICA NO MICRÓSCÓPIO IRÁ PARA ESSA CENA
 NameError: name 'CELULA' is not defined
 '''},
+{'date': 'Tue Sep 22 2020 09:48:35.144 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''<class 'apresentacao'>
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 148
+    laboratorio.vai()
+NameError: name 'laboratorio' is not defined
+'''},
