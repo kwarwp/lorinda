f540691c3d2ba80711882673da3a9496c001f94d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-21 22:26:23 -0300

    Saved samantha/main.py @2020-09-22 01:26

--- samantha/main.py
+++ samantha/main.py
@@ -28,55 +28,55 @@ PAPAD=""
 CAVERNA=""
 FOTO_PAROQUIA=""
  class kkkk():
-    ft= Cena(img=FOTO_PAROQUIA)
-    leao= Cena(img=LEAO)
-    padre= Elemento(img=FALVINHO)
-    f_t= Cena(img=FOTO_PRINCIPAL)
-    dalmacia= Cena(img=DALMACIA)
-    j_j= Elemento(img=JERONIMO_JOVEM)
-    j_v= Elemento(img=JERONIMO_VELHO)
-    batizado= Cena(img=BATIZADO_JERONIMO)
-    roma= Cena(img=ROMA)
-    ordenacao= Cena(img=ORDDENACAO_SACERDOTAL)
-    sonho= Cena(img=SONHO)
-    cr= Cena(img=CURIOSIDADE)
-    caverna= Cena(img=CAVERNA)
-    pl= Elemento(img=PAPA_LIBERIO)
-    j_c= Cena(img=JERONIMO_CAVERNA)
-    biblia= Cena(img=BIBLIA)
-    vulgata= Cena(img=VULGATA)
-    belem= Cena(img=BELEM)
-    f_p= Cena(img=FOTO_DO_PADROEIRO)
-    frase= Cena(img=FRASE)
-    cf= Cena(img=CENA_FINAL)
-    pd= Elemento(img=PAPAD)
+    def __init__(self):
+    self.ft= Cena(img=FOTO_PAROQUIA)
+    self.leao= Cena(img=LEAO)
+    self.padre= Elemento(img=FALVINHO)
+    self.f_t= Cena(img=FOTO_PRINCIPAL)
+    self.dalmacia= Cena(img=DALMACIA)
+    self.j_j= Elemento(img=JERONIMO_JOVEM)
+    self.j_v= Elemento(img=JERONIMO_VELHO)
+    self.batizado= Cena(img=BATIZADO_JERONIMO)
+    self.roma= Cena(img=ROMA)
+    self.ordenacao= Cena(img=ORDDENACAO_SACERDOTAL)
+    self.sonho= Cena(img=SONHO)
+    self.cr= Cena(img=CURIOSIDADE)
+    self.caverna= Cena(img=CAVERNA)
+    self.pl= Elemento(img=PAPA_LIBERIO)
+    self.j_c= Cena(img=JERONIMO_CAVERNA)
+    self.biblia= Cena(img=BIBLIA)
+    self.vulgata= Cena(img=VULGATA)
+    self.belem= Cena(img=BELEM)
+    self.f_p= Cena(img=FOTO_DO_PADROEIRO)
+    self.frase= Cena(img=FRASE)
+    self.cf= Cena(img=CENA_FINAL)
+    self.pd= Elemento(img=PAPAD)
     
-    def entrou_padre(*_):
-        entrou.padre(f_t)
-        padre.vai=Texto(f_t, "olá pessoal, certinho?")
-        padre.vai=Texto(f_t, "Eu sou o Padre Flávio e vou te ajudar nessa aventura sobre a história do nosso amado padroeiro")
+    def entrou_padre(self,*_):
+        self.padre.entra(self.f_t)
+        padre.vai=Texto(self.f_t, "olá pessoal, certinho?")
+        padre.vai=Texto(self.f_t, "Eu sou o Padre Flávio e vou te ajudar nessa aventura sobre a história do nosso amado padroeiro")
         
-    def entrou_1():
-        entrou.padre(dalmacia)
-        padre.vai=Texto(dalmacia, "São Jerônimo nasceu na Dalmácia no ano de 340.")
-        
-        
-    def entrou_2():
-        entrou.padre(roma)
-        padre.vai=Texto(roma, "Após a morte de seus pais, Jerônimo foi para Roma estudar e durante sua permanencia teve um sonho muito importante para sua conversão.")
+    def entrou_1(self,*_):
+        self.padre.entra(self.dalmacia)
+        self.padre.vai=Texto(self.dalmacia, "São Jerônimo nasceu na Dalmácia no ano de 340.")
         
         
-    def entrou_3
-        entrou.padre(sonho)
-        padre.vai=Texto(sonho, "No sonho, Jerônimo apresentava-se como cristão e era repreedindo pelo próprio Cristo por estar faltando com a verdade.")
+    def entrou_2(self,*_):
+        self.padre.entra(self.roma)
+        self.padre.vai=Texto(self.roma, "Após a morte de seus pais, Jerônimo foi para Roma estudar e durante sua permanencia teve um sonho muito importante para sua conversão.")
         
+        
+    def entrou_3(self,*_)
+        self.padre.entra(self.sonho)
+        self.padre.vai=Texto(sonho, "No sonho, Jerônimo apresentava-se como cristão e era repreedindo pelo próprio Cristo por estar faltando com a verdade.")
 
-    def entrou_4
-        entrou.padre(pl)
-        padre.vai=Texto(pl, "Aos 25 anos de idade Jerônimo foi batizado pelo Papa Libério no fim de sua permanencia em Roma.")
+    def entrou_4(self,*_)
+        self.padre.entra(self.p1)
+        self.padre.vai=.vai=Texto(self.pl, "Aos 25 anos de idade Jerônimo foi batizado pelo Papa Libério no fim de sua permanencia em Roma.")
         entrou.padre(fp)
-        padre.vai=Texto(fp, "Aos 25 anos de idade Jerônimo foi batizado pelo Papa Libério no fim de sua permanencia em Roma.")
-        
+        self.padre.vai=Texto(self.fp, "Aos 25 anos de idade Jerônimo foi batizado pelo Papa Libério no fim de sua permanencia em Roma.")
+       
     def entrou_5
         entrou.padre(ordenacao)
         padre.vai=Texto(ordenacao, "Jerônimo foi ordenado sacerdote no ano de 379, retirando-se para dedicar-se ao estudo.")
