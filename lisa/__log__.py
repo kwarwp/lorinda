cd23c4165965b8cf57ffa7903d7ff6cf9bf0985b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-22 19:39:05 -0300

    Saved lisa/__log__.py @2020-09-22 22:39

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -1124,3 +1124,19 @@ NameError: name 'TRYPANOSOMA_CRUZI' is not defined
                                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Sep 22 2020 19:39:04.438 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 198
+    Fase1().vai()
+TypeError: __init__() missing 1 positional argument: maria
+'''},
