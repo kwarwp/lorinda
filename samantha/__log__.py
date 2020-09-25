4712a280854c67726835c1ac53a0c948539bc481
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-25 19:52:33 -0300

    Created samantha/__log__.py @2020-09-25 22:52

---
+++ samantha/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Sep 25 2020 19:52:33.571 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  STYLE=
+         ^
+SyntaxError: invalid syntax
+'''},
