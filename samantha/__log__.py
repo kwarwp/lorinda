66e031d262f87f6f79fbb466e7acbfc3584816d5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-26 08:33:07 -0300

    Saved samantha/__log__.py @2020-09-26 11:33

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -48,3 +48,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sat Sep 26 2020 08:33:02.861 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 30
+  class bala():
+  ^
+IndentationError: unexpected indent
+'''},
