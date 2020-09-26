16b6fe7765dfb15aee075579a6ebb4e8e0e53d8e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-26 12:02:38 -0300

    Saved samantha/__log__.py @2020-09-26 15:02

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -188,3 +188,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sat Sep 26 2020 12:02:37.718 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 54
+  def entrou_padre(self,*_):
+  ^
+IndentationError: unexpected indent
+'''},
