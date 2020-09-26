373166108164bd67dd7c521f360fada946a5ba36
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-26 11:27:58 -0300

    Saved samantha/__log__.py @2020-09-26 14:27

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -90,3 +90,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sat Sep 26 2020 11:27:58.596 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 31
+  self.ft= Cena(img=FOTO_PAROQUIA)
+  ^
+IndentationError: expected an indented block
+'''},
