82392bc945a81eb54f1605b08704f1d13884b71a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-26 11:36:53 -0300

    Saved samantha/main.py @2020-09-26 14:36

--- samantha/main.py
+++ samantha/main.py
@@ -58,7 +58,7 @@ class bala():
         
     def entrou_1(self,*_):
         def resposta(optou):
-            respondeu = dict(
+            respondeu= dict(
             A=Texto(self.ft.p,#é uma descrição da alternativa certo?
             B=Texto(self.ft.p,
             C=Texto(self.ft.p,
