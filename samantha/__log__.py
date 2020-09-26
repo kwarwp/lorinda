24f1b78e0a499aee6640d7f0b5ee0c0679ba0acd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-26 12:20:09 -0300

    Saved samantha/__log__.py @2020-09-26 15:20

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -293,3 +293,10 @@ SyntaxError: invalid syntax
                                                                                                                                                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Sep 26 2020 12:20:08.926 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 62
+  respondeu = dict(
+                                                                                                                                                                                                          ^
+SyntaxError: invalid syntax
+'''},
