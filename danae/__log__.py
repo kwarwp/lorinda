bde4fd4173e28be5627a7b2cfb95b4c047f54619
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-29 12:27:36 -0300

    Created danae/__log__.py @2020-09-29 15:27

---
+++ danae/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Sep 29 2020 12:27:34.341 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from _spy.vitollino.main import Cena, Sala STYLE
+                                                  ^
+SyntaxError: trailing comma not allowed without surrounding parentheses
+'''},
