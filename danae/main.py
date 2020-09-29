a131d0ac5ee57e316558f7acee4e521f6beb27ce
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-29 12:45:55 -0300

    Saved danae/main.py @2020-09-29 15:45

--- danae/main.py
+++ danae/main.py
@@ -1,15 +1,21 @@
 # lorinda.danae.main.py
-from _spy.vitollino.main import Cena, Sala, STYLE
+from _spy.vitollino.main import Cena, Sala, Labirinto, STYLE
 STYLE.update(width=900, height="650px")
-CENAS = "CkepkCR XXQmytH".split()
+CENAS = "CkepkCR XXQmytH nnBZp4Y 1ZCmVlf".split()
+INTER = "1ZCmVlf UGVhUV6"
+SANCT = ""
 CENA = "https://i.imgur.com/%s.jpg"
 PROP = "hB7FFDO"
 
 class TheCave:
     def __init__(self):
         cena = Cena(CENA % CENAS)
+        
         sala = Sala(*[CENA % parede for parede in CENAS]) 
+        atrio = Sala(*[CENA % parede for parede in INTER]) 
+        sanct = Sala(*[CENA % parede for parede in SANCT]) 
         #cena.vai()
+        cave = Labirinto(c=atrio,n=sanct, s=sala)
         sala.norte.vai()
         
         
