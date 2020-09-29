83395ecf3f92484070729e12b938f43d47dbbb98
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-29 17:00:36 -0300

    Saved danae/__log__.py @2020-09-29 20:00

--- danae/__log__.py
+++ danae/__log__.py
@@ -67,3 +67,21 @@ TypeError: __init__() takes 6 positional argument but more were given
                                                    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Sep 29 2020 17:00:35.272 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    TheCave()
+  module <module> line 26
+    self.e_placa = Elemento(self.placa, cena=sanct.leste)
+NameError: name 'Elemento' is not defined
+'''},
