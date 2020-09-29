7d044793ccdf47d5f7752b0b4a6874913b4259f4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-29 17:25:01 -0300

    Saved danae/main.py @2020-09-29 20:25

--- danae/main.py
+++ danae/main.py
@@ -1,18 +1,20 @@
 # lorinda.danae.main.py
-from _spy.vitollino.main import Cena, Sala, Labirinto, STYLE
+from _spy.vitollino.main import Cena, Sala, Labirinto, Elemento, STYLE
 STYLE.update(width=900, height="650px")
 CENAS = "CkepkCR nnBZp4Y 1ZCmVlf W5Q3VcS".split()
 INTER = "XXQmytH UGVhUV6 1ZCmVlf bi4tHyr".split()
 SANCT = "5kwiit6 Bip0ltd jKNasd1 Ac7LD9Z".split()
 CENA = "https://i.imgur.com/%s.jpg"
 CAPEL = "XJTHqUW iiiorD4".split()
-PROP = "hB7FFDO i2jZEzM WwNrwlJ u1bDkus cEJbS0C 4yxhrO0".split()
-
+PROP = "S2td4Uk i2jZEzM WwNrwlJ u1bDkus cEJbS0C".split()  # hB7FFDO RO0oeZI
+MPROP = "Fxc4cCK 4yxhrO0".split()
 class TheCave:
     def __init__(self):
         cena = Cena(CENA % CENAS)
-        self.jero, self.placa, self.cruz , self.grego , self.vulgata , self.livro = [
+        self.jero, self.placa, self.cruz , self.grego , self.vulgata = [
             CENA % obj for obj in PROP]
+        self.pano, self.livro = [
+            CENA % obj for obj in MPROP]
         
         capel = [Cena(CENA % parede) for parede in CAPEL]
         sala = Sala(*[CENA % parede for parede in CENAS]) 
@@ -23,7 +25,11 @@ class TheCave:
         capel[0].meio = capel[1]
         capel[1].meio = capel[1].esquerda = capel[1].direita = sala.norte
         capel[0].vai()
-        self.e_placa = Elemento(self.placa, cena=sanct.leste)
+        atrio.leste.vai()
+        sanct.leste.vai()
+        self.e_placa = Elemento(self.placa, x=510, y=210, w=280, cena=atrio.leste)
+        self.e_jero = Elemento(self.jero, x=360, y=214, w=147, h=250, cena=sanct.leste)
+        self.e_jero = Elemento(self.pano, x=360, y=212, w=150, h=250, cena=sanct.leste)
         
         
 if __name__ == "__main__":
