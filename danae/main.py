4883e2430b7ae73112da8d9862fc4dbc311e21bb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-09-29 12:30:53 -0300

    Saved danae/main.py @2020-09-29 15:30

--- danae/main.py
+++ danae/main.py
@@ -1,14 +1,16 @@
 # lorinda.danae.main.py
-from _spy.vitollino.main import Cena, STYLE
+from _spy.vitollino.main import Cena, Sala, STYLE
 STYLE.update(width=900, height="650px")
-CENAS = "CkepkCR"
+CENAS = "CkepkCR XXQmytH".split()
 CENA = "https://i.imgur.com/%s.jpg"
-
+PROP = "hB7FFDO"
 
 class TheCave:
     def __init__(self):
         cena = Cena(CENA % CENAS)
-        cena.vai()
+        sala = Sala(*[CENA % parede for parede in CENAS]) 
+        #cena.vai()
+        sala.norte.vai()
         
         
 if __name__ == "__main__":
