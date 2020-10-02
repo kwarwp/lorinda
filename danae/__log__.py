a4debb083e5ceee1978a6a3e12ba1fbf6b20505c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-02 20:57:25 -0300

    Saved danae/__log__.py @2020-10-02 23:57

--- danae/__log__.py
+++ danae/__log__.py
@@ -216,3 +216,23 @@ SyntaxError: invalid syntax
                                                                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Oct 02 2020 20:57:24.767 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 275
+    main()
+  module <module> line 271
+    Altares(sala, sala, sala)
+  module <module> line 106
+    self.icone = Elemento(self.jero, x=360, y=214, w=147, h=250, o=1, tit="icone", drag=True)
+AttributeError: 'Altares' object has no attribute 'jero'
+'''},
