14f794c4f88f3bd6ec5396ca293a8561b9493ef8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-02 17:43:31 -0300

    Saved danae/__log__.py @2020-10-02 20:43

--- danae/__log__.py
+++ danae/__log__.py
@@ -126,3 +126,21 @@ AttributeError: 'TheCave' object has no attribute 'e_jerom'
                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Oct 02 2020 17:43:31.175 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 212
+    main()
+  module <module> line 208
+    Altares(sala, sala, sala)
+NameError: name 'sala' is not defined
+'''},
