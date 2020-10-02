357da0fbb9f75e68aede7d2a67335497dbc4564d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-10-02 17:45:32 -0300

    Saved danae/__log__.py @2020-10-02 20:45

--- danae/__log__.py
+++ danae/__log__.py
@@ -162,3 +162,23 @@ NameError: name 'sala' is not defined
     atrio.norte.vai()
 NameError: name 'atrio' is not defined
 '''},
+{'date': 'Fri Oct 02 2020 17:45:31.209 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 212
+    main()
+  module <module> line 208
+    Altares(sala, sala, sala)
+  module <module> line 106
+    self.icone = Elemento(self.jero, x=360, y=214, w=147, h=250, tit="icone", drag=True)
+AttributeError: 'Altares' object has no attribute 'jero'
+'''},
