303ecce4971eb9bfffe58f3fc7e668a8f7c43082
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-13 17:15:02 -0300

    Saved samantha/__log__.py @2020-11-13 20:15

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -575,3 +575,23 @@ SyntaxError: invalid syntax
                                                                                                                                                                                                                                                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Nov 13 2020 17:15:01.174 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 295
+    Bala()
+  module <module> line 62
+    self.entrou_6()
+  module <module> line 180
+    self.damaso = Texto(self.pd,
+AttributeError: 'Bala' object has no attribute 'entrou_7'
+'''},
