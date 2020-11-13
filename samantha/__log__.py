c62bfeb0181e867b88f91562e417f5f3b70593ce
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-13 18:18:25 -0300

    Saved samantha/__log__.py @2020-11-13 21:18

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -615,3 +615,23 @@ AttributeError: 'Bala' object has no attribute 'entrou_7'
     self.sagrada = Texto(self.frase,
 NameError: name 'entrou_final' is not defined
 '''},
+{'date': 'Fri Nov 13 2020 18:18:24.611 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 290
+    Bala()
+  module <module> line 62
+    self.entrou_12()
+  module <module> line 279
+    self.sagrada = Texto(self.frase,
+NameError: name 'entrou_final' is not defined
+'''},
