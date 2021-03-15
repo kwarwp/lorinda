21e73cde63b3faf22b38edf9342f35a9690e7780
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-15 15:18:44 -0300

    Created libby/main.py @2021-03-15 18:18

---
+++ libby/main.py
@@ -0,0 +1,7 @@
+# lorinda.libby.main.
+from _spy.vitollino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import inventario as inv 
+CELULA_1 = "https://img.pebmed.com.br/wp-content/uploads/2018/09/26163836/bacteria-3662695_640-min.jpg"
+CELULA_2 = "https://static.todamateria.com.br/upload/tc/ru/t_cruzi_1.jpg"
+CELULA_3 = "https://img2.gratispng.com/20180602/bga/kisspng-plant-cell-cl-lula-eucariota-cl-lula-animal-animal-cell-5b1365ea94c884.3552609815279979306094.jpg"
+CELULA_5 = "https://www.infoescola.com/wp-content/uploads/2020/01/celula-caliciforme-1029137758.jpg"
