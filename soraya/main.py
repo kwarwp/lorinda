0ed33d89e7b74f54ea40c4f0fb3632b2866ad3f0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-22 14:51:18 -0300

    Created soraya/main.py @2021-03-22 17:51

---
+++ soraya/main.py
@@ -0,0 +1,5 @@
+# lorinda.soraya.main.py
+from _spy.vitollino.main import Cena,Elemento,Texto
+from _spy.vitollino.main
+MARACANA ="https://colunadofla.com/wp-content/uploads/2019/09/maracana-1.jpg"
+PICAPAU ="https://i.pinimg.com/originals/b7/52/36/b752361500109d29744c4d872330d9c7.png"
