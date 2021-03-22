a640ef18c53d27fb4cf90f30507472572f39eb2c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-22 15:20:25 -0300

    Created soraya/__log__.py @2021-03-22 18:20

---
+++ soraya/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Mar 22 2021 15:26:17.465 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  from _spy.vitollino.main
+                          ^
+SyntaxError: invalid syntax
+'''},
