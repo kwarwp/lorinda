49eac8f6b2f3878d9c06223bb75b6a9962599810
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-22 14:59:25 -0300

    Saved soraya/main.py @2021-03-22 17:59

--- soraya/main.py
+++ soraya/main.py
@@ -3,3 +3,5 @@ from _spy.vitollino.main import Cena,Elemento,Texto
 from _spy.vitollino.main
 MARACANA ="https://colunadofla.com/wp-content/uploads/2019/09/maracana-1.jpg"
 PICAPAU ="https://i.pinimg.com/originals/b7/52/36/b752361500109d29744c4d872330d9c7.png"
+VILA_DE_KONOHA ="https://nerdhits.com.br/wp-content/uploads/2020/06/Konohagakure.jpg"
+SONIC ="https://i.pinimg.com/originals/0b/61/b5/0b61b5543bcf94207bb8a9c08a6e99bb.png"
