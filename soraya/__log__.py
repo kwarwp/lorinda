4ea08b600aafac0ade36c2abb5c1d5f292b3d02e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-22 15:21:36 -0300

    Saved soraya/__log__.py @2021-03-22 18:21

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -67,3 +67,21 @@ AttributeError: 'GUI' object has no attribute 'maracana'
     self.picapau.entra(self.maracana)
 AttributeError: 'GUI' object has no attribute 'maracana'
 '''},
+{'date': 'Mon Mar 22 2021 15:27:29.11 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    class teste():
+  module <module> line 14
+    self.picapau.entra(self.maracana)
+AttributeError: 'GUI' object has no attribute 'maracana'
+'''},
