c57ad2d55a2bdd49d9fe1d9505f990d84c443cbc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-25 16:02:01 -0300

    Saved anastasia/main.py @2021-03-25 19:02

--- anastasia/main.py
+++ anastasia/main.py
@@ -1,6 +1,8 @@
 # lorinda.anastasia.main.py
 from _spy.vitollino.main import Jogo, STYLE
 from browser.timer import set_timeout
+"""Usa o timer do navegador para dar um tempinho inicial"""
+
 STYLE.update(width=1350, height="600px")
 J = Jogo()
 """Usa o recurso novo do Vitollino Jogo. Jogo.c é Cena, Jogo.a é Elemento, Jogo.n é Texto"""
@@ -31,6 +33,7 @@ class Associa:
         def __init__(self, nome, tit, x, y, cena, jogo, lacuna=VAZIO, legenda=VAZIO):
             self.nome, self.tit, self.x, self.y, self.jogo = nome, tit, x, y, jogo
             titulo = f"n_{tit}"
+            """Este título serve para marcar cada legenda. É usado pelo drop para conferir se é a legenda certa"""
             drop = {titulo: self.acertou}
             """este dicionário determina que somente a legenda que tenha este título vai acionar o método acertou"""
             self.lacuna = J.a(lacuna, x=x, y=y, w=160, h=40, style=SF, cena=cena, drop=drop)
@@ -72,7 +75,60 @@ class Associa:
         if self.pontua == 4:
             J.n(self.cena, "Você acertou tudo! Parabéns!").vai()
 
+
+
+class Swap:
+    def __init__(self, j, img, cena, w=900,h=400,x=100,y=50,dw=3,dh=3):
+        swap = self
+        class Peca(j.a):
+            def __init__(self, local, indice):
+                self.indice = indice
+                pw, ph = w//dw, h//dh
+                lx, ly = x+local%dw*pw, y+local//dw*ph
+                px, py = indice%dw*pw, indice//dw*ph
+                super().__init__(img, x=lx, y=ly, w=pw, h=ph, drag=True, cena=cena)
+                self.siz = (w, h)
+                # self.elt.Id = f"_swap_{local}"
+                self.elt.Id = f"_swap_{indice}"
+                self.pos = (-px, -py)
+                self.elt.ondrop = lambda ev: self.drop(ev)
+            def drop(self, ev):
+                ev.preventDefault()
+                ev.stopPropagation()
+                src_id = ev.data['text']
+                tit = int(src_id.split('_')[-1])
+                print(f"indice -> {tit}")
+                self.dropped(ev, tit)
+                
+            def dropped(self, ev, indice):
+                print(f"indice, swap -> {indice}", swap.pecas[indice])
+                swap.pecas[indice].pra_la(self, self.x, self.y)
+                swap.montou(indice, self.indice)
+            def pra_la(self, peca, x, y):
+                peca.pra_ca(self.x, self.y)
+                self.x, self.y = x, y
+            def pra_ca(self, x, y):
+                self.x, self.y = x, y
+            def __repr__(self):
+                return str(self.indice)
+
+        from random import shuffle
+        pecas = list(range(dw*dh))
+        self.correto = " ".join(str(pc) for pc in pecas)
+        shuffle(pecas)
+        self.pecas = [Peca(local, indice) for local, indice in enumerate(pecas)]
+        self.resultado = [str(peca) for peca in self.pecas]
+    def montou(self, origem, destino):
+        self.resultado[origem], self.resultado[destino] = self.resultado[destino], self.resultado[origem]
+        resultado = " ".join(self.resultado)
+        print(self.resultado, "<--->", self.correto, "<--->", origem, destino)
+        return resultado == self.correto
+        
+
 def main():
-    Associa()
+    # Associa()
+    cena = J.c(Associa.CENA)
+    Swap(j=J, img=Associa.CELULA, cena=cena, w=600,h=500,x=300,y=50,dw=3,dh=3)
+    cena.vai()
     
 main()
