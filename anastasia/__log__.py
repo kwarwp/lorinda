5b49cb6ddbd9e6e5c2f19c64bbfd46aefa92f3f2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-29 16:14:12 -0300

    Saved anastasia/__log__.py @2021-03-29 19:14

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -377,3 +377,27 @@ Exception: $locals.index is undefined
   module '$exec_2074' line 155
 main()
 '''},
+{'date': 'Mon Mar 29 2021 16:14:11.258 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 155
+    main()
+  module <module> line 152
+    Droner(cena)
+  module <module> line 126
+    self.drone = Drone(self.w+1, cena, self)
+  module <module> line 97
+    self.x, self.y = self.jogo.localiza(index)
+  module _spy.vitollino.main line 684
+    self.elt.style.left = "{}px".format(value)
+AttributeError: 'Drone' object has no attribute 'elt'
+'''},
