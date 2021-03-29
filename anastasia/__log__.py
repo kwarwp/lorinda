c74743e637faac7861b86d9f581fc01cc7516e1f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-29 11:54:19 -0300

    Saved anastasia/__log__.py @2021-03-29 14:54

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -293,3 +293,23 @@ Traceback (most recent call last):
     Rosa = nametuple("Rosa", "norte leste sul oeste")
 NameError: name 'nametuple' is not defined
 '''},
+{'date': 'Mon Mar 29 2021 11:54:18.712 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 105
+    main()
+  module <module> line 102
+    Droner(cena)
+  module <module> line 93
+    set_timeout(self.inicia, "1000")
+AttributeError: 'Droner' object has no attribute 'inicia'
+'''},
