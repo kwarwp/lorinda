b75fa1b1779af0c451833fbac2bf8c7e82fceb82
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-29 11:08:22 -0300

    Saved anastasia/__log__.py @2021-03-29 14:08

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -169,3 +169,31 @@ NameError: name 'Anteparo' is not defined
     super().__init__(img, x=x, y=y, w=pw, h=ph, cena=cena)
 NameError: name 'Anteparo' is not defined
 '''},
+{'date': 'Mon Mar 29 2021 11:08:21.567 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 67
+    main()
+  module <module> line 64
+    Droner(cena)
+  module <module> line 58
+    Droner.Anteparo(100, 100, cena, self)
+  module <module> line 46
+    super().__init__(img, x=x, y=y, w=pw, h=ph, cena=cena)
+NameError: name 'Anteparo' is not defined
+'''},
