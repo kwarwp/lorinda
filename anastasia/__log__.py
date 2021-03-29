13d4077266a40fd7f8833d0b979667058d73d67e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-29 11:51:11 -0300

    Saved anastasia/__log__.py @2021-03-29 14:51

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -277,3 +277,19 @@ Traceback (most recent call last):
   module <module> line 15
     from collections import nametuple
 '''},
+{'date': 'Mon Mar 29 2021 11:51:10.409 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 17
+    Rosa = nametuple("Rosa", "norte leste sul oeste")
+NameError: name 'nametuple' is not defined
+'''},
