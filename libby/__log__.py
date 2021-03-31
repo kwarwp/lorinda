1c3e10bcf1d688739e92f585097e7bc1b50d71fb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-31 14:39:23 -0300

    Created libby/__log__.py @2021-03-31 17:39

---
+++ libby/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Mar 31 2021 14:39:21.774 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 20
+  self.npc= Elemento(img= NPC)
+  ^
+IndentationError: expected an indented block
+'''},
