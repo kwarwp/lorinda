72eafafb181d815ee45bf741ad93d158661e61ad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-31 14:44:07 -0300

    Saved libby/__log__.py @2021-03-31 17:44

--- libby/__log__.py
+++ libby/__log__.py
@@ -37,3 +37,21 @@ Traceback (most recent call last):
   module <module> line 3
     from _spy.vitollino.main import inventario as inv 
 '''},
+{'date': 'Wed Mar 31 2021 14:44:06.973 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 58
+    fase2()
+  module <module> line 36
+    self.npc.entra(self.celula,Tit = "Observe atentamente essas imagens e coloque as letras correspondentes."
+AttributeError: 'fase2' object has no attribute 'npc'
+'''},
