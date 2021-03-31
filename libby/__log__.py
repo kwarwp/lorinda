77dbb18cf324445aadd08a77fd798b898bd503d4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-03-31 15:19:54 -0300

    Saved libby/__log__.py @2021-03-31 18:19

--- libby/__log__.py
+++ libby/__log__.py
@@ -55,3 +55,21 @@ Traceback (most recent call last):
     self.npc.entra(self.celula,Tit = "Observe atentamente essas imagens e coloque as letras correspondentes."
 AttributeError: 'fase2' object has no attribute 'npc'
 '''},
+{'date': 'Wed Mar 31 2021 15:19:54.212 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 60
+    fase2()
+  module <module> line 31
+    self.jogo = Associa(self.membrana)
+TypeError: 'module' object is not callable
+'''},
