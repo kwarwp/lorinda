af2edbe233eb7856b8c2819f80f5682d26007af0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-01 17:05:16 -0300

    Created kellee/__log__.py @2021-04-01 20:05

---
+++ kellee/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Apr 01 2021 17:05:14.204 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  class fase3():
+                ^
+IndentationError: expected an indented block
+'''},
