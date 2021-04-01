a7a5c499dbf038bd0515932798cb155de96662a9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-01 17:15:25 -0300

    Saved kellee/main.py @2021-04-01 20:15

--- kellee/main.py
+++ kellee/main.py
@@ -3,5 +3,13 @@ from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
 from _spy.vitollino.main import Inventario as inv 
 
 
+MARIA = ""
+ROSALINDA = "https://imgur.com/0Dv7w29"
+LABORATORIO = ""
 
 class fase3():
+    def __init__(self):
+    self.laboratorio=Cena(img=LABORATORIO)
+    self.maria=Elemento(img=MARIA, tit"oi,  Dr. Rosalinda sou sua fã, li todos os seus livros e seu artigo sobre "Direcionamento de Proteínas", ou seja, como as proteínas percorrem toda a célula")
+    self.rosalinda=Elemento(img=ROSALINDA)
+    
