d2b08957e422327ce1b6461e374e9eda12ab1a48
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-01 17:18:23 -0300

    Saved kellee/main.py @2021-04-01 20:18

--- kellee/main.py
+++ kellee/main.py
@@ -10,6 +10,7 @@ LABORATORIO = ""
 class fase3():
     def __init__(self):
     self.laboratorio=Cena(img=LABORATORIO)
-    self.maria=Elemento(img=MARIA, tit"oi,  Dr. Rosalinda sou sua fã, li todos os seus livros e seu artigo sobre "Direcionamento de Proteínas", ou seja, como as proteínas percorrem toda a célula")
-    self.rosalinda=Elemento(img=ROSALINDA)
-    
+    self.maria=Elemento(img=MARIA, tit="oi,  Dr. Rosalinda sou sua fã, li todos os seus livros e seu artigo sobre "Direcionamento de Proteínas", ou seja, como as proteínas percorrem toda a célula")
+    self.rosalinda=Elemento(img=ROSALINDA, tit="as proteínas são muito importantes, para a nossa saúde e beleza! Precismos estuda-las, para nos manter saudáveis, fortes e bonitas. ")
+    self.maria.entra(self.laboratorio)
+    self.rosalinda.entra(self.laboratorio)
