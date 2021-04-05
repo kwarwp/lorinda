1d696e6bd65f6bb2e5ede463320f4d2345af5d35
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-05 19:46:01 -0300

    Saved kellee/main.py @2021-04-05 22:46

--- kellee/main.py
+++ kellee/main.py
@@ -1,7 +1,7 @@
 # lorinda.kellee.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
 from _spy.vitollino.main import Inventario as inv 
-
+# salas do games da Angelica, lorinda-lisa-libby e kellee
 
 MARIA = "https://imgur.com/OTFGXEr"
 ROSALINDA = "https://imgur.com/0Dv7w29"
