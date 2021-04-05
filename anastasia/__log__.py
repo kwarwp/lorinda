aa3b54de916da5e629dbacdae10d5984f1c4ba58
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-05 15:38:35 -0300

    Saved anastasia/__log__.py @2021-04-05 18:38

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -616,3 +616,23 @@ SyntaxError: invalid syntax
     associa = Associa(cena)
 TypeError: __init__() missing 1 positional argument: caixa
 '''},
+{'date': 'Mon Apr 05 2021 15:38:34.864 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 195
+    main()
+  module <module> line 192
+    Droner(cena)
+  module <module> line 163
+    self.a = Drone(22, cena, self)
+NameError: name 'Drone' is not defined
+'''},
