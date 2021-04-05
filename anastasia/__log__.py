6faaa988b4bb9f6d4a9ae7a17fabe614b672bece
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-05 15:44:01 -0300

    Saved anastasia/__log__.py @2021-04-05 18:44

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -636,3 +636,25 @@ TypeError: __init__() missing 1 positional argument: caixa
     self.a = Drone(22, cena, self)
 NameError: name 'Drone' is not defined
 '''},
+{'date': 'Mon Apr 05 2021 15:44:00.490 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 196
+    main()
+  module <module> line 193
+    Droner(cena)
+  module <module> line 164
+    self.a = Drone(22, cena, self)
+  module <module> line 135
+    x, y, _ = [(coor + GAP//4)  for coor in (x, y)]
+ValueError: need more than 2 values to unpack
+'''},
