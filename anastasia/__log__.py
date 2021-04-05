1626c516495c47713638a0e23cc8f58ca5949549
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-05 19:04:06 -0300

    Saved anastasia/__log__.py @2021-04-05 22:04

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -658,3 +658,25 @@ NameError: name 'Drone' is not defined
     x, y, _ = [(coor + GAP//4)  for coor in (x, y)]
 ValueError: need more than 2 values to unpack
 '''},
+{'date': 'Mon Apr 05 2021 19:04:06.124 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 199
+    main()
+  module <module> line 196
+    Droner(cena)
+  module <module> line 168
+    self.drone = Drone(0, cena, self)
+  module <module> line 130
+    x, y, azimuth = self.jogo.localiza(index)
+ValueError: too many values to unpack (expected 3)
+'''},
