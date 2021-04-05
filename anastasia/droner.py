4093b07a4d930b0a3f94a4761c2588e71580870a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-05 18:41:36 -0300

    Saved anastasia/droner.py @2021-04-05 21:41

--- anastasia/droner.py
+++ anastasia/droner.py
@@ -104,6 +104,7 @@ class Droner:
                 x, y = randint(1,10), randint(1,4)
                 ax, ay = azimuth = choice(list(ROSA))
                 cx, cy = [10, x, 0][ax+1], [0, y, 5][ay+1]
+                cx, cy = 0, 3
                 cx, cy = GAP+2*GAP*cx, int(-0.5*GAP)+2*GAP*cy
 
                 # print("localiza", cx, cy, azimuth)
