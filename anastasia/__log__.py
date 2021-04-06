e92578caedb8997457f08c2cebddbdb52e722e52
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-06 12:27:28 -0300

    Saved anastasia/__log__.py @2021-04-06 15:27

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -860,3 +860,10 @@ SyntaxError: invalid syntax
                                                                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Apr 06 2021 12:27:27.690 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 118
+  self.partida.azimuth = 0 if (0 < x < 10) else (1 if x == 0 else -1) , 0 if (0 < y < 10) else (1 if y == 0 else -1)
+                                                                       ^
+SyntaxError: invalid syntax
+'''},
