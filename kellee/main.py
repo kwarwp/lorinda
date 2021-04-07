53894f2d478c2d664a1fcd361b9d35b85edc40e1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-07 15:06:14 -0300

    Saved kellee/main.py @2021-04-07 18:06

--- kellee/main.py
+++ kellee/main.py
@@ -14,44 +14,44 @@ DNA= ""
 NPC=""
 class fase3():
     def __init__(self):
-    self.laboratorio=Cena(img=LABORATORIO)
-    self.maria=Elemento(img=MARIA, tit="oi,  Dr. Rosalinda sou sua fã, li todos os seus livros e seu artigo sobre "Direcionamento de Proteínas", ou seja, como as proteínas percorrem toda a célula")
-    self.rosalinda=Elemento(img=ROSALINDA, tit="as proteínas são muito importantes, para a nossa saúde e beleza! Precismos estuda-las, para nos manter saudáveis, fortes e bonitas. ")
-    self.maria.entra(self.laboratorio)
-    self.rosalinda.entra(self.laboratorio)
+        self.laboratorio=Cena(img=LABORATORIO)
+        self.maria=Elemento(img=MARIA, tit="oi,  Dr. Rosalinda sou sua fã, li todos os seus livros e seu artigo sobre "Direcionamento de Proteínas", ou seja, como as proteínas percorrem toda a célula")
+        self.rosalinda=Elemento(img=ROSALINDA, tit="as proteínas são muito importantes, para a nossa saúde e beleza! Precismos estuda-las, para nos manter saudáveis, fortes e bonitas. ")
+        self.maria.entra(self.laboratorio)
+        self.rosalinda.entra(self.laboratorio)
     
     def some (ev):
         self.rosalinda=-100000
-    self.laboratorio_1 = Cena(img= LABORATORIO_1)
-    self.laboratorio.direita=self.laboratorio_1
-    self.laboratorio_1.esquerda=self.laboratorio
-    self.rosalinda.entra(self.laboratorio_1)
-    self.elt.onclick = some
-    self.maria.entra(self.laboratorio_1)
-    self.maria(tit=" Sim, elas são importantes.Então quer dizer que se eu não me alimentar bem, posso ter cabelos, unhas e pele feias?") 
-    self.rosalinda(tit= "sim claro")
-    #rosalinda some 
+        self.laboratorio_1 = Cena(img= LABORATORIO_1)
+        self.laboratorio.direita=self.laboratorio_1
+        self.laboratorio_1.esquerda=self.laboratorio
+        self.rosalinda.entra(self.laboratorio_1)
+        self.elt.onclick = some
+        self.maria.entra(self.laboratorio_1)
+        self.maria(tit=" Sim, elas são importantes.Então quer dizer que se eu não me alimentar bem, posso ter cabelos, unhas e pele feias?") 
+        self.rosalinda(tit= "sim claro")
+        #rosalinda some 
     
     def parte_2():
-    self.laboratorio_2= Cena(img=LABORATORIO_2)
-    self.laboratorio_1.direita=self.laboratorio_2
-    self.laboratorio_2.esquerda=self.laboratorio_1
-    self.maria.entra(self.laboratorio_2, tit= " Como ela desapareceu?")
-    self.npc.entra(self.laboratorio_2)
-    self.npc= Elemento(img=NPC, tit " Você não deve esquecer do seu verdadeiro propósito buscar, desvendar um grande enigma celular. Você deve sempre lembrar que para uma célula funcionar, todas as suas organelas conectadas devem estar. Quando uma proteína conseguir transportar, livre você estará!")
+        self.laboratorio_2= Cena(img=LABORATORIO_2)
+        self.laboratorio_1.direita=self.laboratorio_2
+        self.laboratorio_2.esquerda=self.laboratorio_1
+        self.maria.entra(self.laboratorio_2, tit= " Como ela desapareceu?")
+        self.npc.entra(self.laboratorio_2)
+        self.npc= Elemento(img=NPC, tit " Você não deve esquecer do seu verdadeiro propósito buscar, desvendar um grande enigma celular. Você deve sempre lembrar que para uma célula funcionar, todas as suas organelas conectadas devem estar. Quando uma proteína conseguir transportar, livre você estará!")
     
     def parte_3():
-    self.laboratorio_3=Cena(img=LABORATORIO_3)
-    self.laboratorio_2.direita=self.laboratorio_3
-    self.laboratorio_3.esquerda=self.laboratorio_2
-    self.npc.entra(self.laboratorio_3, tit=" ele não é muito simpático")
-    self.maria.entra(self.laboratorio_3, tit=" quem é você?")
-    self.dna=Elemento(img= DNA)
-    self.dna.entra(self.laboratorio_3, tit="COMO, VOCÊ NÃO SABE MEU NOME?DE QUE PLANETA VOCÊ É?EU SOU O MAIORAL! ")
+        self.laboratorio_3=Cena(img=LABORATORIO_3)
+        self.laboratorio_2.direita=self.laboratorio_3
+        self.laboratorio_3.esquerda=self.laboratorio_2
+        self.npc.entra(self.laboratorio_3, tit=" ele não é muito simpático")
+        self.maria.entra(self.laboratorio_3, tit=" quem é você?")
+        self.dna=Elemento(img= DNA)
+        self.dna.entra(self.laboratorio_3, tit="COMO, VOCÊ NÃO SABE MEU NOME?DE QUE PLANETA VOCÊ É?EU SOU O MAIORAL! ")
     
     def parte_4():
-    self.laboratorio_4=Cena(img=LABORATORIO_4)
-    self.laboratorio_3.direita=self.laboratorio_4
-    self.laboratorio_4.esquerda=self.laboratorio_3
-    self.dna.entra(self.laboratorio_4, tit="vá estudar garota!")
-    self.npc.entra(self.laboratorio_4,tit=" Responda o enigma: Sou constituído por letras, que se encaixam perfeitamente, seguindo uma determinada ordem. Quando tem algum erro, pode provocar uma doença ou mutação.")
+        self.laboratorio_4=Cena(img=LABORATORIO_4)
+        self.laboratorio_3.direita=self.laboratorio_4
+        self.laboratorio_4.esquerda=self.laboratorio_3
+        self.dna.entra(self.laboratorio_4, tit="vá estudar garota!")
+        self.npc.entra(self.laboratorio_4,tit=" Responda o enigma: Sou constituído por letras, que se encaixam perfeitamente, seguindo uma determinada ordem. Quando tem algum erro, pode provocar uma doença ou mutação.")
