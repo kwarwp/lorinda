4a1ee9f84398a146ee96025a6ab0e50ce9a9545b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-07 15:22:41 -0300

    Saved kellee/main.py @2021-04-07 18:22

--- kellee/main.py
+++ kellee/main.py
@@ -2,6 +2,7 @@
 from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
 from _spy.vitollino.main import Inventario as inv 
 # salas do games da Angelica, lorinda-lisa-libby e kellee
+STYLE.update(width=600, height="600px")
 
 MARIA = "https://imgur.com/OTFGXEr.png"
 ROSALINDA = "https://imgur.com/0Dv7w29.png"
@@ -15,22 +16,23 @@ NPC=""
 class fase3():
     def __init__(self):
         self.laboratorio=Cena(img=LABORATORIO)
-        self.maria=Elemento(img=MARIA, tit='oi,  Dr. Rosalinda sou sua fã, li todos os seus livros e seu artigo sobre "Direcionamento de Proteínas", ou seja, como as proteínas percorrem toda a célula')
-        self.rosalinda=Elemento(img=ROSALINDA, tit="as proteínas são muito importantes, para a nossa saúde e beleza! Precismos estuda-las, para nos manter saudáveis, fortes e bonitas. ")
+        self.maria=Elemento(img=MARIA, x=300, y=400, w=180, h=200, tit='oi,  Dr. Rosalinda sou sua fã, li todos os seus livros e seu artigo sobre "Direcionamento de Proteínas", ou seja, como as proteínas percorrem toda a célula')
+        self.rosalinda=Elemento(img=ROSALINDA, x=100, y=400, w=180, h=200, tit="as proteínas são muito importantes, para a nossa saúde e beleza! Precismos estuda-las, para nos manter saudáveis, fortes e bonitas. ")
         self.maria.entra(self.laboratorio)
         self.rosalinda.entra(self.laboratorio)
+        self.laboratorio_1 = Cena(img= LABORATORIO_1)
+        # self.laboratorio.direita=self.laboratorio_1
+        self.laboratorio.direita=Cena(vai=self.some)
         self.laboratorio.vai()
     
-    def some (ev):
-        self.rosalinda=-100000
-        self.laboratorio_1 = Cena(img= LABORATORIO_1)
-        self.laboratorio.direita=self.laboratorio_1
+    def some (self):
+        #self.rosalinda.x=-100000
         self.laboratorio_1.esquerda=self.laboratorio
         self.rosalinda.entra(self.laboratorio_1)
-        self.elt.onclick = some
+        #self.elt.onclick = some
         self.maria.entra(self.laboratorio_1)
-        self.maria(tit=" Sim, elas são importantes.Então quer dizer que se eu não me alimentar bem, posso ter cabelos, unhas e pele feias?") 
-        self.rosalinda(tit= "sim claro")
+        self.maria.tit=" Sim, elas são importantes.Então quer dizer que se eu não me alimentar bem, posso ter cabelos, unhas e pele feias?" 
+        self.rosalinda.tit= "sim claro"
         #rosalinda some 
     
     def parte_2():
