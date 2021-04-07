716b625f14e4f40fbd7dcdde961fff49e3bae724
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-07 15:04:25 -0300

    Saved kellee/__log__.py @2021-04-07 18:04

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -6,3 +6,10 @@
                 ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Apr 07 2021 15:04:24.737 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  self.laboratorio=Cena(img=LABORATORIO)
+  ^
+IndentationError: expected an indented block
+'''},
