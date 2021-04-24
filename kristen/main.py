4dc514876c66fca1117139474fdd8aa9eddf8fea
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-24 17:52:14 -0300

    Created kristen/main.py @2021-04-24 20:52

---
+++ kristen/main.py
@@ -0,0 +1,17 @@
+# lorinda.kristen.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import INVENTARIO as inv 
+
+STYLE["width"] = 900
+STYLE["heigth"] ='650px'
+
+MATHEUS-TIAO = ""
+CALICE  
+AMBULA=""
+PATENA=""
+GALHETA=""
+JARRO-BACIA-MANUSTERGIO=""
+SANGUINEO=""
+CORPORAL=""
+PALA=""
+PAROQUIA
