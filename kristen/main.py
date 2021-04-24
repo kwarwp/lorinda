e67400432d31a7f6b5bc8afb771b2ae148f6bdf2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-24 18:08:26 -0300

    Saved kristen/main.py @2021-04-24 21:08

--- kristen/main.py
+++ kristen/main.py
@@ -6,12 +6,30 @@ STYLE["width"] = 900
 STYLE["heigth"] ='650px'
 
 MATHEUS-TIAO = ""
-CALICE  
-AMBULA=""
-PATENA=""
-GALHETA=""
-JARRO-BACIA-MANUSTERGIO=""
-SANGUINEO=""
-CORPORAL=""
-PALA=""
-PAROQUIA
+CALICE = "" 
+AMBULA = ""
+PATENA = ""
+GALHETA = ""
+JARRO-BACIA-MANUSTERGIO = ""
+SANGUINEO = ""
+CORPORAL = ""
+PALA = ""
+PAROQUIA = ""
+ALTAR = ""
+PRESBITERIO = "https://scontent.fcfb1-1.fna.fbcdn.net/v/t1.6435-9/168540222_4412180955478316_593441197893704662_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=8bfeb9&_nc_eui2=AeGCOzl1cuuTXD_uWyjqV2DDmCHEghmzKhKYIcSCGbMqEnSR9qEKiqw1WlDrkYUaZbWF2P3dN7kHV4mgBvbpkhJ6&_nc_ohc=q9C75Mp_uKQAX-IbCvE&_nc_ht=scontent.fcfb1-1.fna&oh=1e4daad38474aea8803fe038836cc4a1&oe=60AA3B07" 
+
+
+class Tiao:
+    def __init__(self):
+    self.tiao = Elemento(img=MATHEUS_TIAO)
+    self.calice = Elemento 
+    self.ambula = Elemento
+    self.patena = Elemento
+    self.galheta = Elemento
+    self.jarro = Elemento
+    self.sanguineo = Elemento
+    self.corporal = Elemento
+    self.pala = Elemento
+    self.paroquia = Cena
+    self.altar = Cena
+    self.presbiterio = Cena 
