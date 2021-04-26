6fafbfd846d7b409e658fe0659adcc5472625f73
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-26 16:50:26 -0300

    Created kristen/__log__.py @2021-04-26 19:50

---
+++ kristen/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Apr 26 2021 16:50:22.231 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 42
+  self.theus.vai = Texto(self.paroquia,  " ....."  foi=self.cena_1).vai
+                                                    ^
+SyntaxError: invalid syntax
+'''},
