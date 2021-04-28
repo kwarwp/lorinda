5b59d5aaa249fe0932171e9932a3bca7bd89738c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-28 17:32:37 -0300

    Saved libby/__log__.py @2021-04-28 20:32

--- libby/__log__.py
+++ libby/__log__.py
@@ -116,3 +116,27 @@ TypeError: 'module' object is not callable
                                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Apr 28 2021 17:32:36.926 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    from anastasia.main import Associa, SF
+  module anastasia.main line 177
+    main()
+  module anastasia.main line 171
+    mito = associa.nome(nome="mitocôndria", tit=0, x=650, y=150)
+  module anastasia.main line 76
+    return self.Nome(nome=nome, tit=tit, x=x, y=y,caixa=self.caixa, borda=self.borda,
+  module anastasia.main line 36
+    self.acertar = acertou or self.acerta
+AttributeError: 'Nome' object has no attribute 'acerta'
+'''},
