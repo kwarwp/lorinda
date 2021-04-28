4a070059958929989bff4e078941f2d079aff56d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-28 16:38:46 -0300

    Created courtney/__log__.py @2021-04-28 19:38

---
+++ courtney/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Apr 28 2021 16:38:43.987 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  STYLE.update(width=600, height="600px)
+                                        ^
+SyntaxError: EOL while scanning string literal
+'''},
