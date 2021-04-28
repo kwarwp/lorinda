45058d2ec6c087e9d4a1a3496ed61f305a09fa91
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-28 17:05:22 -0300

    Saved courtney/main.py @2021-04-28 20:05

--- courtney/main.py
+++ courtney/main.py
@@ -5,11 +5,41 @@ STYLE.update(width=600, height="600px")
 class Mochila:
     MOCHILA = "https://i.imgur.com/N8eUQ2S.png"
     ATP = "https://i.imgur.com/k0Az1Ts.png"
+    ADP = "https://i.imgur.com/XhUguZt.png"
     def __init__(self):
         self.mochila = Cena(self.MOCHILA)
         self.mochila.vai()
-        self.atp = Elemento(self.ATP, cena=self.mochila)
+        #self.atp = Elemento(self.ATP, cena=self.mochila)
+        #self.adp = Elemento(self.ADP, cena=self.mochila, x=90)
+        self.moedas = []
         
+    def calcula_moeda(self):
+        moedas = len(self.moedas)
+        return dict(x=(moedas % 10) * 60, y=(moedas // 10) * 60)
         
+    def ganha_atp(self):
+        xy = self.calcula_moeda()
+        Elemento(self.ATP, cena=self.mochila, w=50, h=50, **xy)
+        self.moedas.append(self.ATP)
+        
+    def ganha_adp(self):
+        xy = self.calcula_moeda()
+        Elemento(self.ADP, cena=self.mochila, w=50, h=50, **xy)
+        self.moedas.append(self.ADP)
+        
+
+MOCHILA = Mochila()
 if __name__ == "__main__":
-    Mochila()
+    m = Mochila()
+    m.ganha_adp()
+    m.ganha_adp()
+    m.ganha_adp()
+    m.ganha_adp()
+    m.ganha_adp()
+    m.ganha_adp()
+    m.ganha_adp()
+    m.ganha_atp()
+    m.ganha_atp()
+    m.ganha_atp()
+    m.ganha_atp()
+    m.ganha_adp()
