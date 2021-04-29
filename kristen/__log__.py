d602e210d4fe7d818ed7fdba0ceddbc49fd9ade9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-04-28 21:55:03 -0300

    Saved kristen/__log__.py @2021-04-29 00:55

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -90,3 +90,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Apr 28 2021 21:55:01.981 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 44
+  self.theus.vai = Texto(self.paroquia,
+                                                                                                                                                                                                                    ^
+SyntaxError: invalid syntax
+'''},
