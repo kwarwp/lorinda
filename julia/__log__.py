7a7dca00888594f79c86a67e0569b4d7b05cdcc7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-06 12:21:44 -0300

    Saved julia/__log__.py @2021-05-06 15:21

--- julia/__log__.py
+++ julia/__log__.py
@@ -81,3 +81,21 @@ Traceback (most recent call last):
     revista()
 NameError: name 'revista' is not defined
 '''},
+{'date': 'Thu May 06 2021 12:21:44.39 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 49
+    revista1()
+  module <module> line 42
+    self.nena.entra(self.mata, tit="Olá eu sou a Nena, eu amo cuidar do planeta, sou organixa e monitoro tudo, por isso que eu trouxe uma bússula")
+TypeError: entra() got an unexpected keyword argument 'tit'
+'''},
