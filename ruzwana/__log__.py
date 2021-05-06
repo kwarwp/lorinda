ae53fd56f090fac87f7c60db912d27d702e7e091
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-06 15:34:53 -0300

    Saved ruzwana/__log__.py @2021-05-06 18:34

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -81,3 +81,21 @@ AttributeError: 'inventario' object has no attribute 'mostra_corda'
     self.corda_inv.vai=self.mostra_corda
 AttributeError: 'inventario' object has no attribute 'mostra_corda'
 '''},
+{'date': 'Thu May 06 2021 15:34:53.393 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 39
+    MOCHILA=inventario()
+  module <module> line 19
+    self.corda_inv.vai=self.mostra_corda
+AttributeError: 'inventario' object has no attribute 'mostra_corda'
+'''},
