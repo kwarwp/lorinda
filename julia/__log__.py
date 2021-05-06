2b505aa5025e8b59707e4d1ab9ff36884d1c4943
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-06 12:20:31 -0300

    Saved julia/__log__.py @2021-05-06 15:20

--- julia/__log__.py
+++ julia/__log__.py
@@ -27,3 +27,10 @@ SyntaxError: EOL while scanning string literal
                                                                                                                                                                         ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Thu May 06 2021 12:20:31.129 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 49
+  revista():
+            ^
+SyntaxError: invalid syntax
+'''},
