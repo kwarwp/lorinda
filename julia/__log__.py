b4d116501760027e009425163cdb88dd03c8d06f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-06 12:17:23 -0300

    Saved julia/__log__.py @2021-05-06 15:17

--- julia/__log__.py
+++ julia/__log__.py
@@ -13,3 +13,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Thu May 06 2021 12:17:23.421 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 43
+  self.perola.entra(self.mata, tit= "Olá,eu sou a Pérola,eu sou bastante forte e sou da equipe de resgate do meu bairro, por isso eu trouxe o kit de primeiros socorros)
+                                                                                                                                                                        ^
+SyntaxError: EOL while scanning string literal
+'''},
