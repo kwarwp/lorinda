debd3fe9a24224ed13de6fcb142436b16487e5cf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-06 15:13:05 -0300

    Created ruzwana/main.py @2021-05-06 18:13

---
+++ ruzwana/main.py
@@ -0,0 +1,19 @@
+# lorinda.ruzwana.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
+from _spy.vitollino.main import INVENTARIO as inv 
+
+STYLE.update(width=600, height="600px")
+CORDA ="https://i.imgur.com/sD6vJLJ.png"
+FACA =" https://i.imgur.com/T6W0qHH.png"
+FOGO =" https://i.imgur.com/oH55GvU.png"
+KIT =" https://i.imgur.com/ZVrPHcl.png"
+MAPA ="https://i.imgur.com/dXllrys.png"
+BUSSULA =" https://i.imgur.com/5OMt7f2.png"
+class inventario():
+     def __init__(self)
+        self.corda= Elemento(img=CORDA)
+        self.faca= Elemento(img=FACA)
+        self.fogo=Elemento(img=FOGO)
+        self.kit=Elemento(img=KIT)
+        self.mapa=Elemento(img=MAPA)
+        self.bussula=Elemento(img=ELEMENTO)
