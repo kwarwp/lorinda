c6eb306376e067313d6ad1345e4f4b7bde039a30
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-06 15:32:46 -0300

    Saved ruzwana/main.py @2021-05-06 18:32

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -9,11 +9,32 @@ FOGO =" https://i.imgur.com/oH55GvU.png"
 KIT =" https://i.imgur.com/ZVrPHcl.png"
 MAPA ="https://i.imgur.com/dXllrys.png"
 BUSSULA =" https://i.imgur.com/5OMt7f2.png"
+MOCHILA ="https://i.imgur.com/N8eUQ2S.png"
 class inventario():
-     def __init__(self)
+     def __init__(self):
+        self.mochila=Cena(img=MOCHILA)
+        self.mochila.vai()
         self.corda= Elemento(img=CORDA)
+        self.corda_inv= Elemento(img=CORDA)
+        self.corda_inv.vai=self.mostra_corda
+        
         self.faca= Elemento(img=FACA)
+        self.faca_inv= Elemento(img=FACA)
+        self.faca_inv.vai=self.mostra_faca
+        
         self.fogo=Elemento(img=FOGO)
+        self.fogo_inv=Elemento(img=FOGO)
+        self.fogo_inv=self.mostra_fogo
+        
         self.kit=Elemento(img=KIT)
+        self.kit_inv=Elemento(img=KIT)
+        self.kit_inv=self.mostra_kit
+        
         self.mapa=Elemento(img=MAPA)
+        self.mapa_inv=Elemento(img=MAPA)
+        
         self.bussula=Elemento(img=ELEMENTO)
+        self.bussula_inv=Elemento(img=ELEMENTO)
+MOCHILA=inventario()
+
+        
