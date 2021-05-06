148026eb70f5961788a913e3f28ee78f84194b2d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-06 19:22:03 -0300

    Saved ruzwana/main.py @2021-05-06 22:22

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -16,26 +16,27 @@ class inventario:
         self.mochila.vai()
         self.corda= Elemento(img=CORDA)
         self.corda_inv= Elemento(img=CORDA)
-        self.corda_inv.vai=self.mostra_corda
+        # self.corda_inv.vai=self.mostra_corda
         inv.bota(self.corda_inv)
         
         self.faca= Elemento(img=FACA)
         self.faca_inv= Elemento(img=FACA)
-        self.faca_inv.vai=self.mostra_faca
+        # self.faca_inv.vai=self.mostra_faca
         
         self.fogo=Elemento(img=FOGO)
         self.fogo_inv=Elemento(img=FOGO)
-        self.fogo_inv=self.mostra_fogo
+        # self.fogo_inv=self.mostra_fogo
         
         self.kit=Elemento(img=KIT)
         self.kit_inv=Elemento(img=KIT)
-        self.kit_inv=self.mostra_kit
+        #self.kit_inv=self.mostra_kit
         
         self.mapa=Elemento(img=MAPA)
         self.mapa_inv=Elemento(img=MAPA)
         
         self.bussula=Elemento(img=ELEMENTO)
         self.bussula_inv=Elemento(img=ELEMENTO)
+
 MOCHILA=inventario()
 
         
