c4f6abdfb733cc29ef926ca313bc236a6e79bfe1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-06 12:15:17 -0300

    Saved julia/main.py @2021-05-06 15:15

--- julia/main.py
+++ julia/main.py
@@ -8,22 +8,24 @@ PEROLA = " https://i.imgur.com/CWuMMvW.png"
 JACKSON = " https://i.imgur.com/XeNSqrz.png"
 LEILA = " https://i.imgur.com/Vpw3hbY.png"
 MATA = ""
-ESCOLA = ""
+ESCOLA = "https://png.pngtree.com/png-clipart/20190726/ourlarge/pngtree-cartoon-hand-drawn-yellow-school-building-illustration-png-image_1570994.jpg"
 TUCANO = "https://i.imgur.com/mS9ASAx.png"
-CORDA =""
-FACA =""
-FOGO =""
-KIT =""
-MAPA =""
-BUSSULA =""
+CORDA ="https://i.imgur.com/sD6vJLJ.png"
+FACA =" https://i.imgur.com/T6W0qHH.png"
+FOGO =" https://i.imgur.com/oH55GvU.png"
+KIT =" https://i.imgur.com/ZVrPHcl.png"
+MAPA ="https://i.imgur.com/dXllrys.png"
+BUSSULA =" https://i.imgur.com/5OMt7f2.png"
 
 
 class revista1():
     def __init__(self):
-    self.escola=Cena(img=ESCOLA, Texto= "um grupo de amigos estudavam e se divertiam juntos e além disso,  tinham poderes especiais, entretanto por conta de uma pandemia, toda a população humana foi obrigada a viver em isolamento." 
+    self.escola=Cena(img=ESCOLA)
+    Texto(self.escola, "um grupo de amigos estudavam e se divertiam juntos e além disso,  tinham poderes especiais, entretanto por conta de uma pandemia, toda a população humana foi obrigada a viver em isolamento." 
                                         "Após dois anos de confinamento a vacina foi descoberta, após a vacinação em massa, os estudantes voltaram a se encontrar. Os amigos, que moravam mais perto da Mata Atlântica, na cidade de São Paulo"
-                                        "começaram a perceber o sumiço de alguns animais, e se reuniram para desvendar esse mistério!")
-    self.mata=Cena(img=MATA,Texto= "caracteristicas dos personagens , passe  o mouse para descobrir seu personagem")
+                                        "começaram a perceber o sumiço de alguns animais, e se reuniram para desvendar esse mistério!").vai()
+    self.mata=Cena(img=MATA)
+    Texto(self.mata, "caracteristicas dos personagens , click para descobrir seu personagem").vai()
     self.lily=Elemento(img=LILY)
     self.nena=Elemento(img=NENA)
     self.perola=Elemento(img=PEROLA)
@@ -37,9 +39,11 @@ class revista1():
     
     self.escola.direita=self.mata
     self.mata.esquerda=self.escola
-    self.nena.entra(self.mata,)
-    self.perola.entra(self.mata)
-    self.jackson.entra(self.mata)
+    self.nena.entra(self.mata, tit="Olá eu sou a Nena, eu amo cuidar do planeta, sou organixa e monitoro tudo, por isso que eu trouxe uma bússula")
+    self.perola.entra(self.mata, tit= "Olá,eu sou a Pérola,eu sou bastante forte e sou da equipe de resgate do meu bairro, por isso eu trouxe o kit de primeiros socorros)
+    self.lily.entra(self.mata, tit="Olá eu sou a Lily Raquel, eu sou bem otimista e desbravadora, por isso trouxe um facão")
+    self.jackson.entra(self.mata, tit ="Olá eu sou o Jackson,me colocaram como líder do grupo,pois ")
+    self.leila.entra(self.mata)
     
     self.escola.vai()
 revista():
