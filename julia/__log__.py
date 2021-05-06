bb2a69efced0713d080b3c7ece082771bf82c51b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-06 12:57:37 -0300

    Saved julia/__log__.py @2021-05-06 15:57

--- julia/__log__.py
+++ julia/__log__.py
@@ -141,3 +141,21 @@ SyntaxError: invalid syntax
                                                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu May 06 2021 12:57:36.929 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 55
+    revista1()
+  module <module> line 29
+    self.escola.direita=self.mata
+AttributeError: 'revista1' object has no attribute 'mata'
+'''},
