17481e2ca655c6c8e07b74bf3fea9669a0cbe060
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-06 11:27:55 -0300

    Created julia/__log__.py @2021-05-06 14:27

---
+++ julia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu May 06 2021 11:27:53.899 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 23
+  self.escola=Cena(img=ESCOLA, Texto= "um grupo de amigos estudavam e se divertiam juntos e além disso,  tinham poderes especiais, entretanto por conta de uma pandemia, toda a população humana foi obrigada a viver em isolamento." 
+  ^
+IndentationError: expected an indented block
+'''},
