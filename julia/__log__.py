c3e49303ed83e331055e7a02e884ceda8b24ee1d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-06 16:25:37 -0300

    Saved julia/__log__.py @2021-05-06 19:25

--- julia/__log__.py
+++ julia/__log__.py
@@ -207,3 +207,19 @@ SyntaxError: invalid syntax
     self.corda_inv.vai=self.mostra_corda
 AttributeError: 'inventario' object has no attribute 'mostra_corda'
 '''},
+{'date': 'Thu May 06 2021 16:25:36.852 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    from ruzwana.main import MOCHILA
+ImportError: cannot import name 'MOCHILA'
+'''},
