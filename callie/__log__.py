a5d47e6527ab750fa3163fafa0cd5b1f58d24062
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-06 10:13:45 -0300

    Created callie/__log__.py @2021-05-06 13:13

---
+++ callie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu May 06 2021 10:13:43.886 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  DOENTE=
+          ^
+SyntaxError: invalid syntax
+'''},
