c3b17bcd90843c48fd2834525d904f8f246e9702
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-05-06 15:26:47 -0300

    Created ruzwana/__log__.py @2021-05-06 18:26

---
+++ ruzwana/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu May 06 2021 15:26:45.257 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  def __init__(self)
+                     ^
+SyntaxError: invalid syntax
+'''},
