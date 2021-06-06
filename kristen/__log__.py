033183454bb088119ec8ee80caa56ce819648130
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-05 21:46:29 -0300

    Saved kristen/__log__.py @2021-06-06 00:46

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -188,3 +188,10 @@ SyntaxError: invalid syntax
                                                                                                                                                                                                                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Jun 05 2021 21:46:29.111 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 47
+  self.theus.vai = Texto(self.paroquia,
+                                                                                                                                                                                                                 ^
+SyntaxError: invalid syntax
+'''},
