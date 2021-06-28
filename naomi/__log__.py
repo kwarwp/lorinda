b01805dae2a1fd6ac0fed37bc4179cd215729327
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-28 18:04:30 -0300

    Saved naomi/__log__.py @2021-06-28 21:04

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -6,3 +6,10 @@
              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Jun 28 2021 18:04:29.908 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  self.movente = Elemento("https://i.imgur.com/slnDrGI.png", cena=self.cena,
+                                                                                                                                          ^
+SyntaxError: invalid syntax
+'''},
