749ed054b8e3dfc622630a7a050213210cf0601f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-28 17:58:06 -0300

    Created naomi/__log__.py @2021-06-28 20:58

---
+++ naomi/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Jun 28 2021 17:58:04.384 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  class Move
+             ^
+SyntaxError: invalid syntax
+'''},
