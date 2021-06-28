a3f3b6592b54965a227f6fc97902bd826add33b5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-28 18:37:49 -0300

    Saved naomi/main.py @2021-06-28 21:37

--- naomi/main.py
+++ naomi/main.py
@@ -1,13 +1,15 @@
 # lorinda.naomi.main.py
-from _spy.vitollino.main import Cena, Elemento, STYLE
+from _spy.vitollino.main import Cena, Elemento,Texto, STYLE
 STYLE.update(width=1000, height="600px")
 
 class Move:
     def __init__(self):
-        self.cena = Cena("https://i.imgur.com/sGoKfvs.jpg")
-        self.cena.vai()
-        self.movente = Elemento("https://i.imgur.com/slnDrGI.png", cena=self.cena,
-            vai=self.mover, style={"transition": "left 2s"})
+        PROTEINA = "https://i.imgur.com/slnDrGI.png"
+        self.parede = Cena("https://i.imgur.com/sGoKfvs.jpg")
+        self.parede.vai()
+        self.movente = Elemento(img=PROTEINA, cena=self.parede,
+            style={"transition": "left 2s"})
+        Texto(self.parede, "veja a proteina chegar na parede", foi=self.mover).vai()
         
     def mover(self, ev=None):
         self.movente.x=800
