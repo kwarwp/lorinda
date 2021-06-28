cdb75a77cbd4eac17123c8a20abb626cb9fecd1f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-28 18:37:01 -0300

    Saved naomi/__log__.py @2021-06-28 21:37

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
                                                                                                                                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Jun 28 2021 18:37:00.671 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  Texto(self.parede "veja a proteina chegar na parede", foi=self.mover
+                     ^
+SyntaxError: invalid syntax
+'''},
