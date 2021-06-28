938582787f1d4da7350f13c1026d9108f45c13d9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-28 18:37:27 -0300

    Saved naomi/__log__.py @2021-06-28 21:37

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -20,3 +20,21 @@ SyntaxError: invalid syntax
                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Jun 28 2021 18:37:26.555 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 17
+    Move()
+  module <module> line 9
+    self.cena.vai()
+AttributeError: 'Move' object has no attribute 'cena'
+'''},
