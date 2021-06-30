26ebf2e71020c9036d36185824b4d5b821f04405
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-30 20:02:45 -0300

    Saved naomi/__log__.py @2021-06-30 23:02

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -110,3 +110,21 @@ SyntaxError: invalid syntax
     MOCHILA.quando_pega(self.pegou_atp)
 AttributeError: 'Mochila' object has no attribute 'quando_pega'
 '''},
+{'date': 'Wed Jun 30 2021 20:02:44.968 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 79
+    Move()
+  module <module> line 16
+    MOCHILA.esvazia_mochila()
+AttributeError: 'Mochila' object has no attribute 'esvazia_mochila'
+'''},
