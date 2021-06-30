cb228d415c67f7ed48d6e84adbe9bc9b6d263500
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-06-30 19:35:27 -0300

    Saved naomi/__log__.py @2021-06-30 22:35

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -92,3 +92,21 @@ AttributeError: 'Move' object has no attribute 'acabou'
                                                                    ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Jun 30 2021 19:35:26.322 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 71
+    Move()
+  module <module> line 14
+    MOCHILA.quando_pega(self.pegou_atp)
+AttributeError: 'Mochila' object has no attribute 'quando_pega'
+'''},
