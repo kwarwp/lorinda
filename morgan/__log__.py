0e07e2452727aa393be959ae0243da294e40f4db
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-02 17:35:31 -0300

    Saved morgan/__log__.py @2021-07-02 20:35

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -31,3 +31,21 @@ SyntaxError: invalid syntax
     self.escuro = Elemento(ESCURO, x=0, y=0, w=1000, h=600, o=0.92, cena=self.celula)
 UnboundLocalError: local variable 'ESCURO' referenced before assignment
 '''},
+{'date': 'Fri Jul 02 2021 17:35:30.903 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 28
+    Mito()
+  module <module> line 8
+    self.celula = Cena(CELULA)
+UnboundLocalError: local variable 'CELULA' referenced before assignment
+'''},
