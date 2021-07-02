ea69bbaedd38aaa4c018d75c2114cb8bec7593a5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-02 17:34:31 -0300

    Saved morgan/__log__.py @2021-07-02 20:34

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -6,3 +6,10 @@
                                                                                 ^
 SyntaxError: keyword can't be an expression
 '''},
+{'date': 'Fri Jul 02 2021 17:34:30.565 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 24
+  self.glicose = Elemento(GLICOSE, tit=, x=0, y=100, w=100, h=100, cena=self.celula, drag=True)
+                                        ^
+SyntaxError: invalid syntax
+'''},
