753f78d1a57657ca2173998b4843ce596f3f3dec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-02 17:21:23 -0300

    Created morgan/__log__.py @2021-07-02 20:21

---
+++ morgan/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Jul 02 2021 17:21:21.126 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 21
+  self.mitocondria = Elemento(MITOCONDRIA, x=200, y=200, w=600, h=200, self.cena=self.celula)
+                                                                                ^
+SyntaxError: keyword can't be an expression
+'''},
