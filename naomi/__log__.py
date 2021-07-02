98f78cce3ea212583373706201075bd65ed6d21c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-02 17:56:32 -0300

    Saved naomi/__log__.py @2021-07-02 20:56

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -199,3 +199,21 @@ SyntaxError: invalid syntax
                                                                                              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Jul 02 2021 17:56:31.628 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 157
+    Fase7()     
+  module <module> line 136
+    self.glicose = Personagem(GLICOSE, tit="glicose", x=0, y=100,
+TypeError: __init__() got an unexpected keyword argument 'tit'
+'''},
