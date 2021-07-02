ae9544e2784017531d997b1ea2c11704fbb33d9c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-02 13:50:50 -0300

    Saved kellee/__log__.py @2021-07-02 16:50

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -98,3 +98,21 @@ IndentationError: expected an indented block
     self.nucleo=Cena(img=NUCLEO)
 NameError: name 'NUCLEO' is not defined
 '''},
+{'date': 'Fri Jul 02 2021 13:50:49.518 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 138
+    fase4()
+  module <module> line 94
+    self.nucleo=Cena(img=NUCLEO)
+NameError: name 'NUCLEO' is not defined
+'''},
