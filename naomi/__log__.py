17cc0a7af7dfb840b891d8c0f1ae615eebbd698e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-02 18:17:51 -0300

    Saved naomi/__log__.py @2021-07-02 21:17

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -253,3 +253,21 @@ TypeError: __init__() got an unexpected keyword argument 'cena'
     self.maria=Personagem(img=MARIA, x=0, y=400, w=100, h=200, afala=afala, responde=self.ribossomo)
 AttributeError: 'Fase7' object has no attribute 'ribossomo'
 '''},
+{'date': 'Fri Jul 02 2021 18:17:50.894 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 162
+    Fase7()     
+  module <module> line 141
+    self.ribossoma = Personagem(RIBOSSOMA, x=200, y=200, w=80, h=80, afala=afala,
+AttributeError: 'Fase7' object has no attribute 'glicose_fake'
+'''},
