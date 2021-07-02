5b8e754c1c5f3a551dbb6d42e438c1c2fd3e6610
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-02 15:01:35 -0300

    Saved tracy/main.py @2021-07-02 18:01

--- tracy/main.py
+++ tracy/main.py
@@ -24,15 +24,29 @@ COMPLEXOG="https://i.imgur.com/hU2mulx.png" # trocar essa imagem
 RNA= "https://i.imgur.com/khPaSvV.png" # trocar essa imagem
 
 
-class Maria(Elemento):
-    def __init__(img=MARIA, x=300, y=400, w=180, h=200):
-        super().__init__(img=MARIA, x=300, y=400, w=180, h=200)
+class Personagem(Elemento):
+    def __init__(self, img=MARIA, x=300, y=400, w=180, h=200, texto="", responde=None):
+        super().__init__(img=img, x=x, y=y, w=w, h=h)
+        self.texto = texto
+        self.responde = responde
+        
+    def fala(self, cena, responde=None, texto=""):
+        Texto(cena, texto if texto else self.texto, foi=responde if responde else self.responde).vai()
+
 
 
 class Fase3():
     def __init__(self):
         self.laboratorio=Cena(img=LABORATORIO)
-        self.maria = Maria()
+        def rosalinda_fala(_=0):
+            rosalinda = ("Rosalinda: As proteínas são muito importantes, para a nossa saúde e beleza!"
+            " Precismos estuda-las, para nos manter saudáveis, fortes e bonitas. ")
+            Texto(self.laboratorio, rosalinda, foi=self.some).vai()
+            
+        self.maria = Personagem()
+        fala = ('Maria: Oi,  Dr. Rosalinda sou sua fã, li todos os seus livros e seu artigo'
+        ' sobre "Direcionamento de Proteínas", ou seja, como as proteínas percorrem toda a célula')
+        self.maria.fala(self.laboratorio, responde=rosalinda_fala, texto=fala)
         #self.maria=Elemento(img=MARIA, x=300, y=400, w=180, h=200, tit='oi,  Dr. Rosalinda sou sua fã, li todos os seus livros e seu artigo sobre "Direcionamento de Proteínas", ou seja, como as proteínas percorrem toda a célula')
         self.rosalinda=Elemento(img=ROSALINDA, x=100, y=400, w=180, h=200, tit="as proteínas são muito importantes, para a nossa saúde e beleza! Precismos estuda-las, para nos manter saudáveis, fortes e bonitas. ")
         self.maria.entra(self.laboratorio)
@@ -42,7 +56,7 @@ class Fase3():
         self.laboratorio.direita=Cena(vai=self.some)
         self.laboratorio.vai()
     
-    def some (self):
+    def some (self, ev=0):
         def sumir(ev=0):
             self.rosalinda.x=-100000
         self.laboratorio_1.vai()
@@ -52,19 +66,27 @@ class Fase3():
         self.rosalinda.vai = Texto(self.laboratorio_1, fala_rosalinda, foi=sumir).vai
         #self.elt.onclick = some
         self.maria.entra(self.laboratorio_1)
-        self.maria.tit=" Sim, elas são importantes.Então quer dizer que se eu não me alimentar bem, posso ter cabelos, unhas e pele feias?" 
+        maria_texto=("Maria: Sim, elas são importantes. Então quer dizer que se eu não me alimentar bem,"
+        " posso ter cabelos, unhas e pele feias?") 
+        self.maria.fala(self.laboratorio_1,responde=self.parte_2, texto= maria_texto) 
         self.rosalinda.tit= ""
         self.laboratorio_1.direita=Cena(vai=self.parte_2)
         #rosalinda some 
     
-    def parte_2(self):
+    def parte_2(self, ev=0):
         self.laboratorio_2= Cena(img=LABORATORIO_2)
+        texto = ("Roboide: Você não deve esquecer do seu verdadeiro propósito buscar,"
+                 " desvendar um grande enigma celular. Você deve sempre lembrar que para uma célula funcionar,"
+                 " todas as suas organelas conectadas devem estar. Quando uma proteína conseguir transportar,"
+                 " livre você estará!")
         #self.laboratorio_1.direita=self.laboratorio_2
         self.laboratorio_2.vai()
         self.laboratorio_2.esquerda=self.laboratorio_1
         self.maria.entra(self.laboratorio_2)
-        self.maria.tit= " Como ela desapareceu?"
-        self.npc= Elemento(img=NPC,y=400,w=160,h=160, tit=" Você não deve esquecer do seu verdadeiro propósito buscar, desvendar um grande enigma celular. Você deve sempre lembrar que para uma célula funcionar, todas as suas organelas conectadas devem estar. Quando uma proteína conseguir transportar, livre você estará!")
+        maria_texto= "Maria:  Como ela desapareceu?"
+        self.npc= Personagem(img=NPC,y=400,w=160,h=160, texto=texto)
+        self.maria.fala(self.laboratorio_2,responde=self.npc.fala, texto=maria_texto) 
+        
         self.npc.entra(self.laboratorio_2)
         self.laboratorio_2.direita=Cena(vai=self.parte_3)
     
@@ -75,9 +97,10 @@ class Fase3():
         self.laboratorio_3.esquerda=self.laboratorio_2
         self.npc.entra(self.laboratorio_3)
         self.maria.entra(self.laboratorio_3)
-        self.npc.tit=" ele não é muito simpático"
-        self.maria.tit=" quem é você?"
-        self.dna=Elemento(img= DNA, y=450,x=200, tit="COMO, VOCÊ NÃO SABE MEU NOME?DE QUE PLANETA VOCÊ É?EU SOU O MAIORAL! ")
+        self.npc.texto = "Roboide: ele não é muito simpático"
+        maria_texto= "Maria:   quem é você?"
+        self.dna=Personagem(img= DNA, y=450,x=200, responde=self.npc.fala, texto="COMO, VOCÊ NÃO SABE MEU NOME? DE QUE PLANETA VOCÊ É?EU SOU O MAIORAL! ")
+        self.maria.fala(self.laboratorio_3,responde=self.dna.fala, texto=maria_texto) 
         self.dna.entra(self.laboratorio_3)
         self.laboratorio_3.direita=Cena(vai=self.parte_4)
 
