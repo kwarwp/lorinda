99e848d13caab18c9a2f6bca72d5cfb01ff847f3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-06 21:54:24 -0300

    Created amanda/__log__.py @2021-07-07 00:54

---
+++ amanda/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Jul 06 2021 21:54:22.248 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 73
+  t = JOGO.n(cena, 'É isto! A Parede Celular!'
+                                                    ^
+SyntaxError: invalid syntax
+'''},
