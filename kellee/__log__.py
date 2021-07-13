afc957f0515fb740a3f2004f3591816591c275c4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-13 18:51:52 -0300

    Saved kellee/__log__.py @2021-07-13 21:51

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -202,3 +202,21 @@ AttributeError: 'Fase3' object has no attribute 'laboratorio'
     self.foco=Elemento(img=FOCO, x=250, y=280, w=90, h=150, style={"opacity": 0}, vai=self.laboratorio.vai)
 AttributeError: 'Fase3' object has no attribute 'laboratorio'
 '''},
+{'date': 'Tue Jul 13 2021 18:51:51.541 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 185
+    Fase3()
+  module <module> line 33
+    self.foco.vai=self.laboratorio.vai
+AttributeError: 'Fase3' object has no attribute 'laboratorio'
+'''},
