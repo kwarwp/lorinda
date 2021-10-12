9feec15747629c10b1b27c5a731bc0f3d5a72ae4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-10-12 15:12:05 -0300

    Saved callie/__log__.py @2021-10-12 18:12

--- callie/__log__.py
+++ callie/__log__.py
@@ -6,3 +6,10 @@
           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 12 2021 15:12:04.887 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  DOENTE=
+          ^
+SyntaxError: invalid syntax
+'''},
