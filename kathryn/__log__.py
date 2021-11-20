ca6973ec3a037702468d3b5d7ee77b7b0efd6471
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-11-19 23:32:49 -0300

    Saved kathryn/__log__.py @2021-11-20 02:32

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -20,3 +20,10 @@ SyntaxError: invalid syntax
                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Nov 19 2021 22:33:10.945 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from_supy.vitolino.main.import Cena,Elemento,Texto, STYLE
+                           ^
+SyntaxError: invalid syntax
+'''},
