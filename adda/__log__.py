31ed5fdb59c17843bf3a54c018d0ec0398187411
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-01-22 00:15:59 -0300

    Created adda/__log__.py @2022-01-22 03:15

---
+++ adda/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sat Jan 22 2022 00:15:57.668 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 35
+  def Primeira_pergunta()
+                          ^
+SyntaxError: invalid syntax
+'''},
