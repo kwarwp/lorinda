d419961effc1a02fbcc5778c9e70768021f8507f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-01-30 16:12:44 -0300

    Saved alexa/__log__.py @2022-01-30 19:12

--- alexa/__log__.py
+++ alexa/__log__.py
@@ -132,3 +132,21 @@ TypeError: __init__() got an unexpected keyword argument 'norte'
     haha= Labirinto (norte=sala1,leste=sala2,oeste=sala3,sul=sala4)
 TypeError: __init__() got an unexpected keyword argument 'norte'
 '''},
+{'date': 'Sun Jan 30 2022 16:12:41.29 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 21
+    class VINTEDOIS():
+  module <module> line 30
+    haha= Labirinto (norte=sala1,leste=sala2,oeste=sala3,sul=sala4)
+TypeError: __init__() got an unexpected keyword argument 'norte'
+'''},
