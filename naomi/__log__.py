e45d88de101db6171e9f89ed530bd95a2e035b5c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-10 15:53:29 -0300

    Saved naomi/__log__.py @2022-02-10 18:53

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -310,3 +310,21 @@ IndentationError: unexpected indent
     self.maria.fala()
 AttributeError: 'Elemento' object has no attribute 'fala'
 '''},
+{'date': 'Thu Feb 10 2022 15:53:27.907 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 239
+    Fase11()
+  module <module> line 214
+    self.maria=Personagem(img= MARIA,fala=afala)
+TypeError: __init__() got an unexpected keyword argument 'fala'
+'''},
