4f73500eff4dcc1b65842211dc61f794010e8651
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-11 19:26:52 -0300

    Saved stacy/__log__.py @2022-02-11 22:26

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -162,3 +162,21 @@ TypeError: __init__() got an unexpected keyword argument 'fala'
                                                                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Feb 11 2022 19:26:51.99 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 119
+    Reticulo()
+  module <module> line 37
+    self.voltar = voltar or lambda *_:None
+NameError: name 'voltar' is not defined
+'''},
