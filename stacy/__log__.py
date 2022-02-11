820bfae6d2b0c41a3036116b5470ce68845581f7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-11 17:30:01 -0300

    Saved stacy/__log__.py @2022-02-11 20:30

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -133,3 +133,25 @@ SyntaxError: invalid syntax
     Personagem(PROTEINA, tit="PROTEÍNA", x= 400, y=200, w=200, h=300, cena =sala_a.norte,
 TypeError: __init__() got an unexpected keyword argument 'cena'
 '''},
+{'date': 'Fri Feb 11 2022 17:30:00.560 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 94
+    Reticulo()
+  module <module> line 36
+    self.vai_reticulo()
+  module <module> line 44
+    self.norte()
+  module <module> line 59
+    Personagem(PROTEINA, tit="PROTEÍNA", x= 400, y=200, w=200, h=300, cena =sala_a.norte,
+TypeError: __init__() got an unexpected keyword argument 'fala'
+'''},
