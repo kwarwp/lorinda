3f7d36a18b78345740a766c4ffe55780302cc684
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-11 15:12:39 -0300

    Saved libby/__log__.py @2022-02-11 18:12

--- libby/__log__.py
+++ libby/__log__.py
@@ -197,3 +197,21 @@ TypeError: entra() got an unexpected keyword argument 'tit'
     self.celula_1.entra(self.membrana, tit= " Sou uma célula procarionte. Tenho como característica principal, é que meu material genético não está envolto por uma membrana nuclear. Ele fica espalhado pelo citoplasma, em regiões conhecidas como nucleóides, onde fica o DNA circular do tipo cromossômico")
 TypeError: entra() got an unexpected keyword argument 'tit'
 '''},
+{'date': 'Fri Feb 11 2022 15:12:37.925 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 99
+    Fase2()
+  module <module> line 45
+    self.celula_1.entra(self.membrana, Tit= "Sou uma célula procarionte. Tenho como característica principal, é que meu material genético não está envolto por uma membrana nuclear. Ele fica espalhado pelo citoplasma, em regiões conhecidas como nucleóides, onde fica o DNA circular do tipo cromossômico")
+TypeError: entra() got an unexpected keyword argument 'Tit'
+'''},
