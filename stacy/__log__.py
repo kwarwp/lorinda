39d5954b021d80a5719d4caf42dbed006112cca7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-11 16:16:22 -0300

    Saved stacy/__log__.py @2022-02-11 19:16

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -77,3 +77,23 @@ NameError: name 'Sala' is not defined
     Elemrnto("https://i.imgur.com/cTgMqWX.png", cena =sala_b.norte)
 NameError: name 'Elemrnto' is not defined
 '''},
+{'date': 'Fri Feb 11 2022 16:16:19.959 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 24
+    Reticulo()
+  module <module> line 11
+    self.vai_reticulo()
+  module <module> line 19
+    Elemrnto("https://i.imgur.com/cTgMqWX.png", cena =sala_b.norte)
+NameError: name 'Elemrnto' is not defined
+'''},
