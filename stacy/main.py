c56f842635a17e942b12f14ef58ec9b9497e7d3c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-11 16:28:27 -0300

    Saved stacy/main.py @2022-02-11 19:28

--- stacy/main.py
+++ stacy/main.py
@@ -2,6 +2,7 @@
 from _spy.vitollino.main import Cena,Elemento,Texto, Sala, Labirinto, STYLE
 from _spy.vitollino.main import Inventario as inv
 STYLE.update(width=1000, height="600px")
+MARIA = "https://i.imgur.com/FukdPW2.jpg"
 
 RETICULO = ["xMzJZEC wYdLfWl kVrzia6 6IHDCU9".split(), "V961vkS bo5bxUQ ySe17QP lv1Ga2i".split()]
 IM = "https://i.imgur.com/{}.jpg"
@@ -15,8 +16,13 @@ class Reticulo:
         sala_b = Sala(*sala_b_args)
         lab0 = Labirinto(sala_a, sala_b, sala_b, sala_b, sala_b)
         lab1 = Labirinto(sala_b, sala_a, sala_a, sala_a, sala_a)
+        Elemento("https://i.imgur.com/YfSoVKE.png", x= 400, y=200, w=200, h=300, cena =sala_a.oeste)
+        Elemento("https://i.imgur.com/YKNLls2.png", x= 600, y=200, w=200, h=300, cena =sala_a.oeste)
+        Elemento(MARIA, x= 800, y=200, w=200, h=300, cena =sala_a.oeste)
+        Elemento("https://i.imgur.com/EDgO8xF.png", x= 600, w=300, cena =sala_a.oeste)
         #Elemento("https://i.imgur.com/cTgMqWX.png", cena =sala_b.norte)
         Elemento("https://i.imgur.com/YKNLls2.png", x= 500, y=200, w=200, h=300, cena =sala_a.norte)
+        Elemento(MARIA, x= 800, y=200, w=200, h=300, cena =sala_a.norte)
         sala_a.norte.vai()
         Elemento("https://i.imgur.com/h6fl6wy.png", x= 600, w=300, cena =sala_a.norte)
 
