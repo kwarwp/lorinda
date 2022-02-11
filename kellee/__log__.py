66948ad270c8984246e1d414ac32a6cea06c2b2e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-02-11 19:33:11 -0300

    Saved kellee/__log__.py @2022-02-11 22:33

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -255,3 +255,10 @@ SyntaxError: invalid syntax
                                                                                                                                                                                                                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Feb 11 2022 19:33:10.553 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 80
+  self.maria.tit=" Maria o observa atentamente.Maria caminha pelo meio daquele material gelatinoso e se aproxima dele. Ele percebe que está sendo observado e olha com uma cara não muito amigável.Quem é você?" 
+                                                                                                                                                                                                                 ^
+SyntaxError: invalid syntax
+'''},
