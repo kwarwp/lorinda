baadccc3c423e0334dab0e8f9e664b6516a4b0c3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-03-19 23:44:50 -0300

    Saved kellee/main.py @2022-03-20 02:44

--- kellee/main.py
+++ kellee/main.py
@@ -38,10 +38,10 @@ class Fase3():
         #self.rosalinda=Elemento(img=ROSALINDA, x=100, y=400, w=180, h=200, tit="As proteínas são muito importantes para a nossa saúde e beleza! Precisamos estuda-las para nos manter saudáveis, fortes e bonitas. ")
         mtit = ('Oi, Dra. Rosalinda! Vim ao seu laboratório pois sou sua fã!  Admiro seu trabalho sobre a difração dos raio-x e me encanta saber que foi você quem realmente descobriu o formato helicoidal do DNA",' 
         'Quero aprender sobre proteínas, me ajuda?')
-        self.maria=Elemento(img=MARIA, x=300, y=300, w=280, h=300, texto=mtit)
+        self.maria=Elemento(img=MARIA, x=50, y=300, w=280, h=300, texto=mtit)
         rtit = ("As proteínas são muito importantes para a nossa saúde e beleza!"
         "Precisamos estuda-las para nos manter saudáveis, fortes e bonitas. ")
-        self.rosalinda=Elemento(img=ROSALINDA, x=50, y=300, w=280, h=300, texto=rtit)
+        self.rosalinda=Elemento(img=ROSALINDA, x=300, y=300, w=280, h=300, texto=rtit)
         self.maria.entra(self.laboratorio)
         self.rosalinda.entra(self.laboratorio)
         #self.foco.vai=self.laboratorio.vai
