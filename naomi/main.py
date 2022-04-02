d236c3dd7a5c7e8df5993838de9d6fa5311f5e51
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-02 09:44:54 -0300

    Saved naomi/main.py @2022-04-02 12:44

--- naomi/main.py
+++ naomi/main.py
@@ -181,12 +181,15 @@ class Fase7():
         self.maria.x = 1500
         self.maria_double.x = 1500
         self.maria_double.elt.style.transform = "rotate(200deg)"
-    LABORATORIO="https://i.imgur.com/g3wt0Vb.jpg"
-    def laboratorio(self):
-        self.laboratorio=Cena =(LABORATORIO)
-        self.maria.entra(self.laboratorio)
+  
     def main(self,_=0):
         self.parede=cena = JOGO.c('https://i.imgur.com/ujAF00x.jpg').vai()
+        self.maria.entra(self.parede)
+        self.ncp.entra(self.parede)
+        afala = "Maria: Nossa! Que estrutura é essa?"
+        afala = "Npc: É uma estrutura que envolve a membrana plasmática, e está presente em células vegetais, organismos procariotose alguns eucariotos, como fungos. Tem como principal função, proteger a célula."
+        afala = "Maria: E agora como faço para sair daqui?"
+        afala = "Npc: Junte seus ATPs. Você precisa resolver o enigma. Coloque as letras corretas e livres você estará."
         t = JOGO.n(cena, 'É isto! A Parede Celular!',foi=self.maria_sai)
         Swap(JOGO, PAREDE, cena, w=700,h=200,x=50,y=150,dw=7,dh=2, venceu=t) 
     
