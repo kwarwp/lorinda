9e4c353a7359d2601c5ef66e66708b0d9751cb8d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-10 22:31:52 -0300

    Saved kellee/__log__.py @2022-04-11 01:31

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -404,3 +404,21 @@ SyntaxError: invalid syntax
                                                                                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sun Apr 10 2022 22:31:52.301 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 206
+    Fase5()
+  module <module> line 167
+    self.rosalinda=Elemento(img=ROSALINDA, x=o, y=350, w=200, h=200, tit ="Maria, olhe a estrutura", cena=self.organela)
+NameError: name 'o' is not defined
+'''},
