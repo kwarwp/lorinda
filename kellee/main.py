d1643d05a20e0d1fd0af60f6693605c77d76d0dd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-20 22:49:55 -0300

    Saved kellee/main.py @2022-04-21 01:49

--- kellee/main.py
+++ kellee/main.py
@@ -83,7 +83,7 @@ class Fase3():
         self.laboratorio_3.esquerda=self.laboratorio_2
         self.npc.entra(self.laboratorio_3)
         self.maria.entra(self.laboratorio_3)
-        self.npc.tit=" Maria ele não é muito simpático. É o todo poderoso! E se acha"
+        self.npc.texto=" Maria ele não é muito simpático. É o todo poderoso! E se acha"
         self.maria.tit="Maria o observa atentamente,Maria caminha pelo meio daquele material gelatinoso e se aproximadele.Ele percebe que está sendo observado e olha com uma cara não muito amigavel.quem é você?"
         self.dna=Elemento(img= DNA, x=450,y=300,w=200, h=200, tit="COMO, VOCÊ NÃO SABE MEU NOME!?DE QUE PLANETA VOCÊ É?!EU SOU O MAIORAL!! ")
         self.dna.entra(self.laboratorio_3)
