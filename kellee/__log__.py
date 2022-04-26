7f0f1229e1f358934e8e6b46ebae818b26cb1658
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-25 22:12:00 -0300

    Saved kellee/__log__.py @2022-04-26 01:12

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -501,3 +501,21 @@ NameError: name 'some' is not defined
     self.laboratorio_3.esquerda=self.laboratorio_2
 AttributeError: 'Fase3' object has no attribute 'laboratorio_2'
 '''},
+{'date': 'Mon Apr 25 2022 22:11:59.955 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 245
+    Fase3().parte_3()
+  module <module> line 124
+    self.laboratorio_3.esquerda=self.laboratorio_2
+AttributeError: 'Fase3' object has no attribute 'laboratorio_2'
+'''},
