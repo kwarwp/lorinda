14146bc7eb432ca27c536bde75ac5d0081b58787
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-25 22:06:18 -0300

    Saved kellee/__log__.py @2022-04-26 01:06

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -483,3 +483,21 @@ UnboundLocalError: local variable 'maria_falou' referenced before assignment
     self.maria=Elemento(img=MARIA, x=100, y=280, w=280, h=300, texto=mtit, foi=some)
 NameError: name 'some' is not defined
 '''},
+{'date': 'Mon Apr 25 2022 22:06:17.564 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 241
+    Fase3().parte_3()
+  module <module> line 119
+    self.laboratorio_3.esquerda=self.laboratorio_2
+AttributeError: 'Fase3' object has no attribute 'laboratorio_2'
+'''},
