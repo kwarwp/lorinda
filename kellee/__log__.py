8b94dc9e746d79f1d90d979c8304a1dcf6c5b090
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-04-25 21:14:45 -0300

    Saved kellee/__log__.py @2022-04-26 00:14

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -429,3 +429,21 @@ NameError: name 'o' is not defined
                                              ^
 SyntaxError: non-keyword arg after keyword arg
 '''},
+{'date': 'Mon Apr 25 2022 21:14:44.804 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 210
+    Fase3()
+  module <module> line 35
+    vai = self.rosalinda.vai
+AttributeError: 'Fase3' object has no attribute 'rosalinda'
+'''},
