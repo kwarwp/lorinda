a69877d621f53cdf0b69b98f650037a27b886941
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-06 14:04:39 -0300

    Created sarah/main.py @2022-06-06 17:04

---
+++ sarah/main.py
@@ -0,0 +1,84 @@
+# lorinda.sarah.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
+from collections import namedtuple
+Ator = namedtuple('Elenco','ator nome mini alinha')
+Fala = namedtuple('Fala','ator fala prox age')  # , defaults=(None,)*4)
+A = namedtuple('Ali','e m d')(-1, 0, 1)
+
+
+class Roteiro:
+    def __init__(self, cena, roteiro, elenco=(), foi=None):
+        self.dic_ator = {a.ator: a for a in elenco}
+        _prox = zip(roteiro, roteiro[1:] + [Fala(None, "", None, None)])
+        self.foi = foi if foi else lambda *_: None
+        roteiro = [Fala(a, f, g if g else (p.ator if p else None), x) for [a, f, g, x], p in _prox]
+        # print(list(prox))
+        # print(roteiro)
+        # return
+        self.elenco, self.roteiro = elenco, roteiro
+        self._foi = lambda *_: None
+        script = self
+        for _ator in elenco:
+            _ator.ator.vai = self.nada
+            _ator.ator.tit = _ator.nome
+            _ator.ator.elt.style.filter = "brightness(30%)"
+        protagonista = elenco[0].ator if elenco else roteiro[0].ator
+        self.atores = [ator.ator for ator in elenco] if elenco else [ator.ator for ator in roteiro]
+        protagonista.vai = self.segue
+        protagonista.elt.style.filter = "brightness(100%)"
+
+        class Falar(Texto):
+            def __init__(self, ator, fala, prox, act=None, mini=1, **kwarg):
+                self.ator, self.fala, self.prox = ator, fala, prox
+                self._foi = act or self.nada
+                minih = 80 / mini
+                self.mini = Elemento(ator.img, cena=cena, w=80, h=80, tipo=f"80px {minih}px",
+                                     style=dict(top="20%", margin="-10px 10%"))
+                super().__init__(cena, fala, **kwarg)
+
+            def esconde(self, *_):
+                self.mini.elt.remove()
+                self.ator.elt.style.filter = "brightness(30%)"
+                script.testa(self.prox)
+                # script.segue()
+                # super().esconde()
+                self._foi()
+
+            def vai(self, *_):
+                super().vai()
+                self.ator.elt.style.filter = "brightness(5%)"
+                self.ator.vai = self.nada
+
+            @property
+            def foi(self):
+                return self._foi
+
+            @foi.setter
+            def foi(self, value):
+                self._foi = value
+
+            def nada(self, *_):
+                pass
+
+        self._fala = Falar
+
+    def nada(self, *_):
+        pass
+
+    def segue(self, *_):
+        ator, fala, prox, action = self.scripter()
+        # ator.elt.style.filter = "brightness(30%)"
+        fala = self._fala(ator, fala, prox, action, mini=self.dic_ator[ator].mini)  # .vai()
+        if prox:
+            prox.vai = self.segue
+        fala.vai()
+
+    def testa(self, prox, *_):
+        if self.roteiro:
+            prox.elt.style.filter = "brightness(100%)"
+        else:
+            for ato in self.atores:
+                ato.elt.style.filter = "brightness(100%)"
+
+    def scripter(self, *_):
+        return self.roteiro.pop(0)
