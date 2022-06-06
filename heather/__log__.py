f97d4f3887862d13654edad2652649398b3d5e80
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-06 12:52:53 -0300

    Saved heather/__log__.py @2022-06-06 15:52

--- heather/__log__.py
+++ heather/__log__.py
@@ -53,3 +53,21 @@ NameError: name 'atores' is not defined
     JOGO.rt(c, rot, ele, self.some)
 AttributeError: 'Jogo' object has no attribute 'rt'
 '''},
+{'date': 'Mon Jun 06 2022 12:52:53.95 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 298
+    Fase3()
+  module <module> line 57
+    nome_ator = zip( atores, nomes, atores[1:]+[None])
+NameError: name 'atores' is not defined
+'''},
