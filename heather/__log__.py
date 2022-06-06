f5c7bff2b91349f5995369b9d6662892044aed19
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-06 12:46:37 -0300

    Created heather/__log__.py @2022-06-06 15:46

---
+++ heather/__log__.py
@@ -0,0 +1,19 @@
+
+{'date': 'Mon Jun 06 2022 12:46:35.591 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 298
+    Fase3()
+  module <module> line 55
+    ele = [Ator(self.maria,"Maria", 0.2, A.e),
+NameError: name 'A' is not defined
+'''},
