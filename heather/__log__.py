a33927d2e584cdf1eb6b8f2a48cd182c687b8b29
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-06 19:15:36 -0300

    Saved heather/__log__.py @2022-06-06 22:15

--- heather/__log__.py
+++ heather/__log__.py
@@ -254,3 +254,21 @@ SyntaxError: invalid syntax
     d = self.dna=Elemento(img=DNA, x=300,y=0, w=200, h=200, texto= "Só eu trabalho aqui? Vou ter que criar um RNA para me ajudar.", foi=dna_falou)
 NameError: name 'dna_falou' is not defined
 '''},
+{'date': 'Mon Jun 06 2022 19:15:36.64 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 242
+    Fase5()
+  module <module> line 200
+    r = self.rosalinda=Elemento(img=ROSALINDA, x=0, y=350, w=200, h=200,foi=rose_falou, texto ="Maria, olhe a estrutura ao seu lado.", cena=self.organela)
+NameError: name 'rose_falou' is not defined
+'''},
