f69d94c103ec179ae9ee93185722578be10a37ce
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-06 23:49:13 -0300

    Saved libby/main.py @2022-06-07 02:49

--- libby/main.py
+++ libby/main.py
@@ -87,7 +87,7 @@ class Fase2():
             self.maria.o = 1
             self.maria.vai = self.atravessou_membrana
         self.membrana = Cena(MEMBRANA).vai()
-        fala = Texto(self.membrana, "Tenho que atravessar a membrana plasmática para entrar na célula.", foi=maria_anda)
+        fala = Texto(self.membrana, "Tenho que atravessar a membrana plasmática para entrar na célula. Ainda bem que estou bem pequenininha e não possuo carga.", foi=maria_anda)
         self.maria = Elemento(img= MARIA, x=200, y=50, w=150, h=120, cena=self.membrana, vai=fala.vai,
         style= {"transition": "top 5s, opacity 5s"})
         self.maria.elt.ontransitionend = maria_chega
