770d42de4b381e631bd21931cef760ffa6630943
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-07 09:45:27 -0300

    Saved heather/__log__.py @2022-06-07 12:45

--- heather/__log__.py
+++ heather/__log__.py
@@ -272,3 +272,21 @@ NameError: name 'dna_falou' is not defined
     r = self.rosalinda=Elemento(img=ROSALINDA, x=0, y=350, w=200, h=200,foi=rose_falou, texto ="Maria, olhe a estrutura ao seu lado.", cena=self.organela)
 NameError: name 'rose_falou' is not defined
 '''},
+{'date': 'Tue Jun 07 2022 09:45:26.985 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 259
+    Fase3()
+  module <module> line 49
+    self.placa = Elemento(ROSALIND, x=250, y=60, w=200,h=70, cena=c)
+NameError: name 'ROSALIND' is not defined
+'''},
