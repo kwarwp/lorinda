ce370a80a4c92f81e4080f60909fd7f2b74d7e83
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-08 09:20:15 -0300

    Saved naomi/__score__.py @2022-06-08 12:20

--- naomi/__score__.py
+++ naomi/__score__.py
@@ -1,1 +1,2 @@
   {'doc_id': 'b89c8548', 'carta': 'parte_1', 'casa': 'Fase7', 'move': 'local', 'ponto': 0, 'valor': 'local', '_level': 1, 'tempo': 1654690808546},
+  {'doc_id': 'b89c8548', 'carta': 'parte_6', 'casa': 'Fase7', 'move': 'local', 'ponto': 0, 'valor': 'local', '_level': 1, 'tempo': 1654690815105},
