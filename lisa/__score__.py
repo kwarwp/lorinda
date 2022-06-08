b2e0a26be4515b3d0f2c98af1c8e525addd82472
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-08 09:59:56 -0300

    Saved lisa/__score__.py @2022-06-08 12:59

--- lisa/__score__.py
+++ lisa/__score__.py
@@ -1043,3 +1043,4 @@
   {'doc_id': '466892f9', 'carta': 'parte_1', 'casa': 'Fase0', 'move': 'local', 'ponto': 0, 'valor': 'local', '_level': 1, 'tempo': 1654693085598},
   {'doc_id': '466892f9', 'carta': 'parte_1', 'casa': 'Fase0', 'move': 'local', 'ponto': 0, 'valor': 'local', '_level': 1, 'tempo': 1654693085695},
   {'doc_id': '8278a2be', 'carta': 'parte_6', 'casa': 'Fase7', 'move': 'local', 'ponto': 0, 'valor': 'local', '_level': 1, 'tempo': 1654693086809},
+  {'doc_id': '8278a2be', 'carta': 'parte_3', 'casa': 'Fase7', 'move': 'local', 'ponto': 0, 'valor': 'local', '_level': 1, 'tempo': 1654693095109},
