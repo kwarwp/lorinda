0261dc308b9093564d14a40264b29813d4750451
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-08 09:01:57 -0300

    Saved libby/__score__.py @2022-06-08 12:01

--- libby/__score__.py
+++ libby/__score__.py
@@ -4,3 +4,4 @@
   {'doc_id': 'af1631e8', 'carta': 'parte_1', 'casa': 'Fase2', 'move': 'local', 'ponto': 0, 'valor': 'local', '_level': 1, 'tempo': 1654689699623},
   {'doc_id': 'af1631e8', 'carta': 'parte_2', 'casa': 'Fase2', 'move': 'local', 'ponto': 0, 'valor': 'local', '_level': 1, 'tempo': 1654689702377},
   {'doc_id': 'af1631e8', 'carta': 'parte_3', 'casa': 'Fase2', 'move': 'local', 'ponto': 0, 'valor': 'local', '_level': 1, 'tempo': 1654689715507},
+  {'doc_id': 'af1631e8', 'carta': 'parte_0', 'casa': 'Fase3', 'move': 'local', 'ponto': 0, 'valor': 'local', '_level': 1, 'tempo': 1654689717308},
