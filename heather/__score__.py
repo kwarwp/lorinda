c3719aa81cda5082b361833982da78b1702d2fe0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-08 08:37:41 -0300

    Saved heather/__score__.py @2022-06-08 11:37

--- heather/__score__.py
+++ heather/__score__.py
@@ -99,3 +99,4 @@
   {'doc_id': 'b80b4990', 'carta': 'parte_0', 'casa': 'Fase4', 'move': 'local', 'ponto': 0, 'valor': 'local', '_level': 1, 'tempo': 1654688253893},
   {'doc_id': 'b80b4990', 'carta': 'parte_0', 'casa': 'Fase4', 'move': 'dialogo', 'ponto': 1, 'valor': 'MDDRD@7', '_level': 1, 'tempo': 1654688255501},
   {'doc_id': 'b80b4990', 'carta': 'parte_0', 'casa': 'Fase4', 'move': 'dialogo', 'ponto': 2, 'valor': 'MDDRD@7', '_level': 1, 'tempo': 1654688257678},
+  {'doc_id': 'b80b4990', 'carta': 'parte_0', 'casa': 'Fase4', 'move': 'dialogo', 'ponto': 3, 'valor': 'MDDRD@7', '_level': 1, 'tempo': 1654688260662},
