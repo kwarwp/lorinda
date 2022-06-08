64a1c35d9e86ea42d4775b8ff72efb34b7cfcf60
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-08 07:30:07 -0300

    Created sarah/__log__.py @2022-06-08 10:30

---
+++ sarah/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Jun 08 2022 07:30:06.26 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  def score(self, casa, carta, move, ponto, valor, _level=1):
+  ^
+IndentationError: unexpected indent
+'''},
