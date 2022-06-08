e9b4472a4670c77a195f131ce2bea553b03c2dc4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-08 07:30:17 -0300

    Saved sarah/__log__.py @2022-06-08 10:30

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Jun 08 2022 07:30:17.453 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 78
+  score = dict(casa=casa, carta=carta, move="dialogo", ponto=self.rotsiz-len(self.roteiro), valor=self.rotid):
+                                                                                                              ^
+SyntaxError: invalid syntax
+'''},
