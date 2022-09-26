b977d92dc32e1f834afcff8856524532fa96284c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-09-26 09:59:20 -0300

    Create __init__.py

---
+++ samantha/__init__.py
@@ -0,0 +1,4 @@
+# This file is part of  program LABASE
+# Copyright © 2022  Carlo Oliveira <carlo@nce.ufrj.br>,
+# `Labase <http://labase.selfip.org/>`__; `GPL <http://is.gd/3Udt>`__.
+# SPDX-License-Identifier: (GPLv3-or-later AND LGPL-2.0-only) WITH bison-exception
